64e00217124bd9381342b07e5917c5eaef8586a9
jalal246 <jimmy002020@gmail.com>
2020-05-21 19:38:48 +0200

    Add more strict test

--- packages/dom-gen/test/generator.ascending.test.js
+++ packages/dom-gen/test/generator.ascending.test.js
@@ -2,29 +2,34 @@ import Generator from "../src/Generator";
 
 const domGen = new Generator();
 
+let pointerChild0D0;
+let pointerChild1D0;
+let pointerChild2D0;
+
+const KEYS_CHILDREN_D0 = {
+  chK: null,
+  pK: "1-0",
+  sK: "0-0",
+};
+
+let pointerParent0D1;
+let pointerGrandParent0D2;
+
+let pointerChild3D0;
+
 describe("DOM Relationship Generator: Ascending-Simple", () => {
   describe("Working on same level: zero(0) depth", () => {
-    const expectedKeys = {
-      chK: null,
-      pK: "1-0",
-      sK: "0-0",
-    };
-
-    const PARENT_ZERO = 0;
-
-    let pointer;
-
     it("Adds new element starting at depth zero(0)", () => {
       // DOM-root
       // │
       // │───id-0  => (order:{parent: 0, self: 0 }) || (keys: {chK: null,pK: "1-0",sK: "0-0"})
 
-      pointer = domGen.getElmPointer("id-0", 0);
+      pointerChild0D0 = domGen.getElmPointer("id-0", 0);
 
-      expect(pointer).toStrictEqual({
-        keys: expectedKeys,
+      expect(pointerChild0D0).toStrictEqual({
+        keys: KEYS_CHILDREN_D0,
         order: {
-          parent: PARENT_ZERO,
+          parent: 0,
           self: 0,
         },
       });
@@ -37,13 +42,13 @@ describe("DOM Relationship Generator: Ascending-Simple", () => {
       // │
       // │───id-1  => (order:{parent: 0, self: 1 }) || (keys: {chK: null,pK: "1-0",sK: "0-0"})
 
-      pointer = domGen.getElmPointer("id-1", 0);
+      pointerChild1D0 = domGen.getElmPointer("id-1", 0);
 
-      expect(pointer).toStrictEqual({
-        keys: expectedKeys,
+      expect(pointerChild1D0).toStrictEqual({
+        keys: KEYS_CHILDREN_D0,
         order: {
-          parent: PARENT_ZERO,
-          self: 1,
+          parent: pointerChild0D0.order.parent,
+          self: pointerChild0D0.order.self + 1,
         },
       });
 
@@ -55,26 +60,20 @@ describe("DOM Relationship Generator: Ascending-Simple", () => {
       // │
       // │───id-2  => (order:{parent: 0, self: 2 }) || (keys: {chK: null,pK: "1-0",sK: "0-0"})
 
-      pointer = domGen.getElmPointer("id-2", 0);
+      pointerChild2D0 = domGen.getElmPointer("id-2", 0);
 
-      expect(pointer).toStrictEqual({
-        keys: expectedKeys,
+      expect(pointerChild2D0).toStrictEqual({
+        keys: KEYS_CHILDREN_D0,
         order: {
-          parent: PARENT_ZERO,
-          self: 2,
+          parent: pointerChild1D0.order.parent,
+          self: pointerChild1D0.order.self + 1,
         },
       });
     });
   });
 
   describe("Works on parents in higher depth", () => {
-    it("Adds new elements as parent", () => {
-      const expectedKeys = {
-        chK: "0-0",
-        pK: "2-0",
-        sK: "1-0",
-      };
-
+    it("Identifies parent connects it with right node children with sk/pk", () => {
       // DOM-root
       // ├───id-parent-1 (order:{parent: 0, self: 0 })   || (keys: {chK: "0-0",pK: "2-0",sK: "1-0"})
       //     |
@@ -84,10 +83,18 @@ describe("DOM Relationship Generator: Ascending-Simple", () => {
       //     │
       //     │───id-2 => ..
 
-      const pointer = domGen.getElmPointer("id-parent-1", 1);
+      pointerParent0D1 = domGen.getElmPointer("id-parent-1", 1);
 
-      expect(pointer).toStrictEqual({
-        keys: expectedKeys,
+      expect(pointerChild0D0.keys.pK).toBe(pointerParent0D1.keys.sK);
+      expect(pointerChild1D0.keys.pK).toBe(pointerParent0D1.keys.sK);
+      expect(pointerChild2D0.keys.pK).toBe(pointerParent0D1.keys.sK);
+
+      expect(pointerParent0D1).toStrictEqual({
+        keys: {
+          chK: "0-0",
+          pK: "2-0",
+          sK: "1-0",
+        },
         order: {
           parent: 0,
           self: 0,
@@ -95,7 +102,7 @@ describe("DOM Relationship Generator: Ascending-Simple", () => {
       });
     });
 
-    it("Adds new elements as grand parent", () => {
+    it("Identifies grand parent connects its branch", () => {
       // DOM-root
       // ├───id-grand-parent-1  (order:{parent: 0, self: 0 }) ||  (keys: {chK: "1-0",pK: "3-0",sK: "2-0"})
       //     |
@@ -107,16 +114,16 @@ describe("DOM Relationship Generator: Ascending-Simple", () => {
       //         │
       //         │───id-2 => ..
 
-      const pointer = domGen.getElmPointer("id-grand-parent-1", 2);
+      pointerGrandParent0D2 = domGen.getElmPointer("id-grand-parent-1", 2);
 
-      const expectedKeys = {
-        chK: "1-0",
-        pK: "3-0",
-        sK: "2-0",
-      };
+      expect(pointerParent0D1.keys.pK).toBe(pointerGrandParent0D2.keys.sK);
 
-      expect(pointer).toStrictEqual({
-        keys: expectedKeys,
+      expect(pointerGrandParent0D2).toStrictEqual({
+        keys: {
+          chK: "1-0",
+          pK: "3-0",
+          sK: "2-0",
+        },
         order: {
           parent: 0,
           self: 0,
@@ -126,7 +133,7 @@ describe("DOM Relationship Generator: Ascending-Simple", () => {
   });
 
   describe("Add new node starting from zero again", () => {
-    it("Adds new elements as grand parent", () => {
+    it("Connects the child with its parent matching index and key", () => {
       // DOM-root
       // ├───id-grand-parent-1  (order:{parent: 0, self: 0 }) ||  (keys: {chK: "1-0",pK: "3-0",sK: "2-0"})
       //     |
@@ -140,16 +147,20 @@ describe("DOM Relationship Generator: Ascending-Simple", () => {
       //
       // ├───id-00  (order:{parent: 1, self: 0 }) ||  (keys: {chK: null,pK: "1-1",sK: "0-1"})
 
-      const pointer = domGen.getElmPointer("id-00", 0);
+      pointerChild3D0 = domGen.getElmPointer("id-00", 0);
 
-      const expectedKeys = {
-        chK: null,
-        pK: "1-1",
-        sK: "0-1",
-      };
+      expect(pointerChild3D0.keys).not.toBe(KEYS_CHILDREN_D0);
 
-      expect(pointer).toStrictEqual({
-        keys: expectedKeys,
+      expect(pointerChild3D0.order.parent).not.toBe(
+        pointerChild0D0.order.parent
+      );
+
+      expect(pointerChild3D0).toStrictEqual({
+        keys: {
+          chK: null,
+          pK: "1-1",
+          sK: "0-1",
+        },
         order: {
           parent: 1,
           self: 0,
