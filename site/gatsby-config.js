c196cb9d3dae6c92e778c042af1bce2b2770543f
jalal246 <jimmy002020@gmail.com>
2020-05-27 21:24:33 +0200

    fix default name

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -25,8 +25,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `DFlex`,
+        short_name: `DFlex`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
