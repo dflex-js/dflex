bda410a28f7b11ce065ae156a1dcc00e1815307b
jalal246 <jimmy002020@gmail.com>
2020-05-28 17:13:40 +0200

    Add eslint config

---
+++ site/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["airbnb", "prettier", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": 0,
+    "react/jsx-filename-extension": 0,
+    "react/prop-types": 0,
+  },
+};
