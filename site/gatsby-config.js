d5488fb6ee2f92cb98b2730c1b44f8a99efcff95
jalal246 <jimmy002020@gmail.com>
2020-05-30 01:24:44 +0200

    FIx background color

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -14,8 +14,8 @@ module.exports = {
         name: `DFlex`,
         short_name: `DFlex`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: "#40241a",
+        theme_color: "#40241a",
         display: `minimal-ui`,
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
