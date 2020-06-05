9b0a614d7b37cc257886cb33ce2107cb4fd7ce0d
jalal246 <jimmy002020@gmail.com>
2020-06-05 17:45:49 +0200

    Add Base for mechanism

---
+++ packages/mechanisms/src/Base/Base.js
@@ -0,0 +1,239 @@
+import store from "@dflex/store";
+
+import { DRAGGED_ELM, ACTIVE_PARENT } from "../constants";
+
+/**
+ * Base element.
+ *
+ * Creates draggedElm and activeParent and initializes thresholds.
+ *
+ *
+ * @class Base
+ */
+class Base {
+  /**
+   * Creates an instance of Base.
+   *
+   * @param {string} elementId
+   * @memberof Base
+   */
+  constructor(elementId) {
+    const { element, parent, siblingsList, parentsList } = store.getDndObjById(
+      elementId
+    );
+
+    this.parentsList = parentsList;
+    this.siblingsList = siblingsList;
+
+    this.setIsSingleton();
+
+    /**
+     * Thresholds store, contains max value for each parent and for dragged. Depending on
+     * ids as keys.
+     */
+    this.thresholds = {
+      parents: {},
+      dragged: {},
+    };
+
+    this.assignDragged(element);
+
+    /**
+     * Dragged has no parent.
+     */
+    this.isDraggedOrphan = true;
+
+    /**
+     * Not all elements have parents.
+     */
+    if (parent) {
+      /**
+       * Indicator to parents that have changed. This facilitates looping in
+       * affected parents only.
+       */
+      this.setOfTransformedIds = new Set([]);
+      this.assignActiveParent(parent);
+      this.setThreshold(parent, true);
+      this.isDraggedOrphan = false;
+    }
+  }
+
+  /**
+   * Check if dragged has no siblings and then set the related flag.
+   *
+   * @memberof Base
+   */
+  setIsSingleton() {
+    /**
+     * Dragged has no siblings.
+     */
+    this.isSingleton = !(this.siblingsList.constructor === Array);
+  }
+
+  /**
+   * Sets thresholds for dragged element position depending on its
+   * position inside parent which is related to droppable left and top.
+   *
+   * maxDirection = current position + droppable-allowed spaces
+   *
+   * @memberof Base
+   */
+  setThreshold(droppable, isParent) {
+    const { parents, dragged } = this.thresholds;
+
+    const {
+      thresholdOffset: { vertical, horizontal },
+    } = this[DRAGGED_ELM];
+
+    const {
+      currentLeft,
+      currentTop,
+      offset: { width, height },
+      id,
+    } = droppable;
+
+    const $ = isParent ? (parents[id] = {}) : dragged;
+
+    /**
+     * Overview:
+     *
+     * this.maxDirection: contains max value allowed for dragged to move. If dragged passes
+     * these values, that means it's out.
+     *
+     * This threshold is related to droppable offset. If droppable changes, then
+     * max/threshold changes.
+     *
+     * parentThreshold: to detect if dragged is moved inside or outside
+     * parent. But what if we have multiple lists?
+     *
+     * Updating positionMax when dragged is located in new position is easy but
+     * in lists we need to know all listMax(s) before entering to parent. Why?
+     * because in this way we know where dragged is to detect the activeParent.
+     *
+     * The solution is to create parentThreshold. An object stores positionMax
+     * depending on currentIndex as keys.
+     */
+
+    /**
+     * Calculate max-vertical for up and down:
+     */
+
+    /**
+     * When going up, currentTop decreases (-vertical).
+     */
+    $.maxTop = currentTop - vertical;
+
+    /**
+     * When going down, currentTop increases (+vertical) with droppable
+     * taking into considerations (+height + vertical).
+     */
+    $.maxBottom = currentTop + height + vertical;
+
+    /**
+     * When going left, currentLeft decreases (-horizontal).
+     */
+    $.maxLeft = currentLeft - horizontal;
+
+    /**
+     * When going right, currentLeft increases (+horizontal) with droppable
+     * taking into considerations (+width + horizontal).
+     */
+    $.maxRight = currentLeft + width + horizontal;
+  }
+
+  /**
+   * Assigns DRAGGED_ELM
+   *
+   * @param {CoreInstance} coreInstance
+   * @memberof Base
+   */
+  assignDragged(coreInstance) {
+    /**
+     * Assign instance for dragged.
+     */
+    this[DRAGGED_ELM] = coreInstance;
+
+    /**
+     * Not initiating thresholdOffset for all coreInstances. Only ones which
+     * dragged will be initiated.
+     */
+    if (!this[DRAGGED_ELM].thresholdOffset) {
+      const {
+        offset: { width, height },
+      } = this[DRAGGED_ELM];
+
+      /**
+       * Calculates thresholdOffset only for dragged element.
+       *
+       * Two-thirds of the dragged element's space for vertical and horizontal. If
+       * two-thirds of the dragged is out, then trigger isOut whether it is out
+       * position or out parent.
+       */
+      this[DRAGGED_ELM].thresholdOffset = {
+        vertical: Math.ceil((2 / 3) * height),
+        horizontal: Math.ceil((2 / 3) * width),
+      };
+    }
+
+    /**
+     * Init max direction for position
+     */
+    this.setThreshold(this[DRAGGED_ELM]);
+  }
+
+  /**
+   * Add parent id to setOfTransformedIds. The goal here, is to avoid any
+   * unnecessary process the parent goes through it when it's flagged as
+   * transformed.
+   *
+   * @memberof Base
+   */
+  addParentAsTransformed() {
+    const { id } = this[ACTIVE_PARENT];
+
+    /**
+     * Avoid adding same parents more than once using sets.
+     */
+    this.setOfTransformedIds.add(id);
+  }
+
+  /**
+   * Assigns new ACTIVE_PARENT: parent who contains dragged
+   *
+   * @param {CoreInstance} coreInstance
+   * @memberof Base
+   */
+  assignActiveParent(coreInstance) {
+    // console.log("%c inside assignActiveParent", "background: lightblue");
+
+    /**
+     * Assign instance ACTIVE_PARENT which represents droppable. Then
+     * assign owner parent so we have from/to.
+     */
+    this[ACTIVE_PARENT] = coreInstance;
+
+    /**
+     * Add flag for undo method so we can check which  parent is being
+     * transformed and which is not.
+     */
+    this[ACTIVE_PARENT].isTransformed = true;
+    this.isDraggedOutActiveParent = false;
+
+    /**
+     * In initiating we get siblingsList. But, when dragged is in another
+     * ACTIVE_PARENT we have different siblingsList now. siblingsList is
+     * children of new parent. Anyway, check if there no list, get us one.
+     */
+    if (!this.siblingsList) {
+      const {
+        keys: { chK },
+      } = this[ACTIVE_PARENT];
+
+      this.siblingsList = store.getListByKey(chK);
+    }
+
+    this.setIsSingleton();
+  }
+}
+
+export default Base;
