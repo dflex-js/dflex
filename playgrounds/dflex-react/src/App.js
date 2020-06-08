04f285ffb972d5682a84dca2775b39b6a1d7e1fb
jalal246 <jimmy002020@gmail.com>
2020-06-08 18:41:09 +0200

    remove logo and workers

--- playgrounds/dflex-react/src/App.js
+++ playgrounds/dflex-react/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import Core from "./Core";
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
