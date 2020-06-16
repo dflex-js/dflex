5abd73ef81351e02857c4e033177272a44faa5b5
jalal246 <jimmy002020@gmail.com>
2020-06-16 19:47:37 +0200

    Add docs

--- packages/coreInstance/src/AbstractCoreInstance.js
+++ packages/coreInstance/src/AbstractCoreInstance.js
@@ -1,7 +1,10 @@
 class AbstractCoreInstance {
   /**
+   * This is the link between store and core element
+   *
    * @param {string} id
    * @param {node} elm
+   * @param {Object} rest
    */
   constructor({ id, element, ...rest }) {
     this.id = id;
