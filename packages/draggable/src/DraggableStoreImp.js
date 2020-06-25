1905672e4687d354f3555bfef9b799b0981225c2
jalal246 <jimmy002020@gmail.com>
2020-06-26 00:21:05 +0200

    Add docs to DraggableStoreImp
    (Should be extended later)

--- packages/draggable/src/DraggableStoreImp.js
+++ packages/draggable/src/DraggableStoreImp.js
@@ -2,6 +2,14 @@ import AbstractCoreInstance from "@dflex/core-instance/src/AbstractCoreInstance"
 import AbstractStore from "@dflex/store/src/AbstractStore";
 
 class DraggableStoreImp extends AbstractStore {
+  /**
+   * Register element for Draggable store
+   *
+   * @param {Object} elmInstance
+   * @param {string} elmInstance.id
+   * @param {node} elmInstance.element
+   * @memberof DraggableStoreImp
+   */
   register(elmInstance) {
     super.register(
       Object.assign(elmInstance, {
