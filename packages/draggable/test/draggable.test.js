113c6c9dd646c4b4f8b41ccf91eac468dd2011f1
jalal246 <jimmy002020@gmail.com>
2020-06-27 00:23:53 +0200

    Initates phase two

--- packages/draggable/test/draggable.test.js
+++ packages/draggable/test/draggable.test.js
@@ -49,27 +49,27 @@ describe("Draggable Store", () => {
   });
 });
 
-const START_CLIENT_X = 10;
-const START_CLIENT_Y = 20;
-
-const MOVING_PIXELS = 50;
-
-let draggable;
-
 describe("Draggable mechanism", () => {
-  beforeAll(() => {
-    draggable = new Draggable(elmInstance1.id, {
-      x: START_CLIENT_X,
-      y: START_CLIENT_Y,
-    });
-  });
-
   describe("FIRST ROUND", () => {
     let EXPECTED_TRANSLATE_X = 0;
     let EXPECTED_TRANSLATE_Y = 0;
 
+    const START_CLIENT_X = 10;
+    const START_CLIENT_Y = 20;
+
     const EXPECTED_OFFSET_X = -START_CLIENT_X + EXPECTED_TRANSLATE_X;
     const EXPECTED_OFFSET_Y = -START_CLIENT_Y + EXPECTED_TRANSLATE_Y;
+
+    const MOVING_PIXELS = 50;
+
+    let draggable;
+
+    beforeAll(() => {
+      draggable = new Draggable(elmInstance1.id, {
+        x: START_CLIENT_X,
+        y: START_CLIENT_Y,
+      });
+    });
 
     describe("Stimulates mousedown click - Inits draggable", () => {
       it("Initiates translateX/Y", () => {
@@ -112,6 +112,66 @@ describe("Draggable mechanism", () => {
       it("Updates style to normal", () => {
         expect(draggable.draggedStyle.pointerEvents).toBe("");
       });
+    });
+  });
+
+  describe("SECOND ROUND", () => {
+    const EXPECTED_TRANSLATE_X = 0;
+    const EXPECTED_TRANSLATE_Y = 0;
+
+    const START_CLIENT_X = 10;
+    const START_CLIENT_Y = 20;
+
+    const EXPECTED_OFFSET_X = -START_CLIENT_X + EXPECTED_TRANSLATE_X;
+    const EXPECTED_OFFSET_Y = -START_CLIENT_Y + EXPECTED_TRANSLATE_Y;
+
+    const MOVING_PIXELS = 50;
+
+    let draggable;
+
+    beforeAll(() => {
+      draggable = new Draggable(elmInstance1.id, {
+        x: START_CLIENT_X,
+        y: START_CLIENT_Y,
+      });
+    });
+
+    describe("Stimulates mousedown click - Inits draggable", () => {
+      it("Initiates translateX/Y", () => {
+        // expect(draggable.draggedElm.translateX).toEqual(EXPECTED_TRANSLATE_X);
+        // expect(draggable.draggedElm.translateY).toEqual(EXPECTED_TRANSLATE_Y);
+      });
+      it("Calculates offset", () => {
+        // expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X);
+        // expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y);
+      });
+    });
+
+    describe("Stimulates mousemove - Checks dragAt()", () => {
+      // beforeAll(() => {
+      //   for (let i = 0; i < MOVING_PIXELS; i += 1) {
+      //     draggable.dragAt(START_CLIENT_X + i, START_CLIENT_Y + i);
+      //     EXPECTED_TRANSLATE_X = START_CLIENT_X + i + EXPECTED_OFFSET_X;
+      //     EXPECTED_TRANSLATE_Y = START_CLIENT_Y + i + EXPECTED_OFFSET_Y;
+      //   }
+      // });
+      // it("Offset never change", () => {
+      //   expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X);
+      //   expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y);
+      // });
+      // it("Draggable updates translateX/Y in store", () => {
+      //   expect(draggable.draggedElm.translateX).toEqual(EXPECTED_TRANSLATE_X);
+      //   expect(draggable.draggedElm.translateY).toEqual(EXPECTED_TRANSLATE_Y);
+      // });
+    });
+
+    describe("Stimulates mouseup - Checks end()", () => {
+      // beforeAll(() => {
+      //   draggable.end();
+      // });
+      // it("Updates style to normal", () => {
+      //   expect(draggable.draggedStyle.pointerEvents).toBe("");
+      // });
     });
   });
 });
