f838a103e7d4ceadf49d343bbc437a96ba17b4e6
jalal246 <jimmy002020@gmail.com>
2020-06-27 02:11:07 +0200

    Regiter elements before starting test

--- packages/draggable/test/draggable.test.js
+++ packages/draggable/test/draggable.test.js
@@ -20,15 +20,15 @@ function addElement() {
 const elmInstance1 = addElement();
 const elmInstance2 = addElement();
 
+store.register(elmInstance1);
+store.register(elmInstance2);
+
 describe("Draggable Store", () => {
-  it("Has empty registry", () => {
-    expect(store.registry).toStrictEqual({});
+  it("Registry is not empty", () => {
+    expect(store.registry).toBeTruthy();
   });
 
   it("Registers element and initiates translateX,Y", () => {
-    store.register(elmInstance1);
-    store.register(elmInstance2);
-
     const result = {
       [elmInstance1.id]: {
         element: elmInstance1.element,
@@ -51,56 +51,71 @@ describe("Draggable Store", () => {
 
 describe("Draggable mechanism", () => {
   describe("FIRST ROUND", () => {
-    let EXPECTED_TRANSLATE_X = 0;
-    let EXPECTED_TRANSLATE_Y = 0;
+    let EXPECTED_TRANSLATE_X_R1 = store.registry[elmInstance1.id].translateX;
+    let EXPECTED_TRANSLATE_Y_R1 = store.registry[elmInstance1.id].translateY;
 
-    const START_CLIENT_X = 10;
-    const START_CLIENT_Y = 20;
+    const START_CLIENT_X_R1 = 10;
+    const START_CLIENT_Y_R1 = 20;
 
-    const EXPECTED_OFFSET_X = -START_CLIENT_X + EXPECTED_TRANSLATE_X;
-    const EXPECTED_OFFSET_Y = -START_CLIENT_Y + EXPECTED_TRANSLATE_Y;
+    const EXPECTED_OFFSET_X_R1 = -START_CLIENT_X_R1 + EXPECTED_TRANSLATE_X_R1;
+    const EXPECTED_OFFSET_Y_R1 = -START_CLIENT_Y_R1 + EXPECTED_TRANSLATE_Y_R1;
 
-    const MOVING_PIXELS = 50;
+    const MOVING_PIXELS_R1 = 50;
 
     let draggable;
 
     beforeAll(() => {
       draggable = new Draggable(elmInstance1.id, {
-        x: START_CLIENT_X,
-        y: START_CLIENT_Y,
+        x: START_CLIENT_X_R1,
+        y: START_CLIENT_Y_R1,
       });
     });
 
     describe("Stimulates mousedown click - Inits draggable", () => {
-      it("Initiates translateX/Y", () => {
-        expect(draggable.draggedElm.translateX).toEqual(EXPECTED_TRANSLATE_X);
-        expect(draggable.draggedElm.translateY).toEqual(EXPECTED_TRANSLATE_Y);
+      it("TranslateX/Y are zeros", () => {
+        expect(EXPECTED_TRANSLATE_X_R1).toEqual(0);
+        expect(EXPECTED_TRANSLATE_Y_R1).toEqual(0);
+      });
+
+      it("matches values in draggable with those in store", () => {
+        expect(draggable.draggedElm.translateX).toEqual(
+          EXPECTED_TRANSLATE_X_R1
+        );
+        expect(draggable.draggedElm.translateY).toEqual(
+          EXPECTED_TRANSLATE_Y_R1
+        );
       });
 
       it("Calculates offset", () => {
-        expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X);
-        expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y);
+        expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X_R1);
+        expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y_R1);
       });
     });
 
     describe("Stimulates mousemove - Checks dragAt()", () => {
       beforeAll(() => {
-        for (let i = 0; i < MOVING_PIXELS; i += 1) {
-          draggable.dragAt(START_CLIENT_X + i, START_CLIENT_Y + i);
+        for (let i = 0; i < MOVING_PIXELS_R1; i += 1) {
+          draggable.dragAt(START_CLIENT_X_R1 + i, START_CLIENT_Y_R1 + i);
 
-          EXPECTED_TRANSLATE_X = START_CLIENT_X + i + EXPECTED_OFFSET_X;
-          EXPECTED_TRANSLATE_Y = START_CLIENT_Y + i + EXPECTED_OFFSET_Y;
+          EXPECTED_TRANSLATE_X_R1 =
+            START_CLIENT_X_R1 + i + EXPECTED_OFFSET_X_R1;
+          EXPECTED_TRANSLATE_Y_R1 =
+            START_CLIENT_Y_R1 + i + EXPECTED_OFFSET_Y_R1;
         }
       });
 
       it("Offset never change", () => {
-        expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X);
-        expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y);
+        expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X_R1);
+        expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y_R1);
       });
 
       it("Draggable updates translateX/Y in store", () => {
-        expect(draggable.draggedElm.translateX).toEqual(EXPECTED_TRANSLATE_X);
-        expect(draggable.draggedElm.translateY).toEqual(EXPECTED_TRANSLATE_Y);
+        expect(draggable.draggedElm.translateX).toEqual(
+          EXPECTED_TRANSLATE_X_R1
+        );
+        expect(draggable.draggedElm.translateY).toEqual(
+          EXPECTED_TRANSLATE_Y_R1
+        );
       });
     });
 
@@ -116,62 +131,83 @@ describe("Draggable mechanism", () => {
   });
 
   describe("SECOND ROUND", () => {
-    const EXPECTED_TRANSLATE_X = 0;
-    const EXPECTED_TRANSLATE_Y = 0;
+    let EXPECTED_TRANSLATE_X_R2 = store.registry[elmInstance1.id].translateX;
+    let EXPECTED_TRANSLATE_Y_R2 = store.registry[elmInstance1.id].translateY;
 
-    const START_CLIENT_X = 10;
-    const START_CLIENT_Y = 20;
+    const START_CLIENT_X_R2 = 100;
+    const START_CLIENT_Y_R2 = 80;
 
-    const EXPECTED_OFFSET_X = -START_CLIENT_X + EXPECTED_TRANSLATE_X;
-    const EXPECTED_OFFSET_Y = -START_CLIENT_Y + EXPECTED_TRANSLATE_Y;
+    const EXPECTED_OFFSET_X_R2 = -START_CLIENT_X_R2 + EXPECTED_TRANSLATE_X_R2;
+    const EXPECTED_OFFSET_Y_R2 = -START_CLIENT_Y_R2 + EXPECTED_TRANSLATE_Y_R2;
 
-    const MOVING_PIXELS = 50;
+    const MOVING_PIXELS_R2 = 18;
 
     let draggable;
 
     beforeAll(() => {
       draggable = new Draggable(elmInstance1.id, {
-        x: START_CLIENT_X,
-        y: START_CLIENT_Y,
+        x: START_CLIENT_X_R2,
+        y: START_CLIENT_Y_R2,
       });
     });
 
     describe("Stimulates mousedown click - Inits draggable", () => {
-      it("Initiates translateX/Y", () => {
-        // expect(draggable.draggedElm.translateX).toEqual(EXPECTED_TRANSLATE_X);
-        // expect(draggable.draggedElm.translateY).toEqual(EXPECTED_TRANSLATE_Y);
+      it("TranslateX/Y are zeros", () => {
+        console.log(store.registry[elmInstance1.id].translateX);
+        expect(EXPECTED_TRANSLATE_X_R2).toEqual(0);
+        expect(EXPECTED_TRANSLATE_Y_R2).toEqual(0);
       });
-      it("Calculates offset", () => {
-        // expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X);
-        // expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y);
+
+      it.skip("matches values in draggable with those in store", () => {
+        expect(draggable.draggedElm.translateX).toEqual(
+          EXPECTED_TRANSLATE_X_R2
+        );
+        expect(draggable.draggedElm.translateY).toEqual(
+          EXPECTED_TRANSLATE_Y_R2
+        );
+      });
+
+      it.skip("Calculates offset", () => {
+        expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X_R2);
+        expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y_R2);
       });
     });
 
-    describe("Stimulates mousemove - Checks dragAt()", () => {
-      // beforeAll(() => {
-      //   for (let i = 0; i < MOVING_PIXELS; i += 1) {
-      //     draggable.dragAt(START_CLIENT_X + i, START_CLIENT_Y + i);
-      //     EXPECTED_TRANSLATE_X = START_CLIENT_X + i + EXPECTED_OFFSET_X;
-      //     EXPECTED_TRANSLATE_Y = START_CLIENT_Y + i + EXPECTED_OFFSET_Y;
-      //   }
-      // });
-      // it("Offset never change", () => {
-      //   expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X);
-      //   expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y);
-      // });
-      // it("Draggable updates translateX/Y in store", () => {
-      //   expect(draggable.draggedElm.translateX).toEqual(EXPECTED_TRANSLATE_X);
-      //   expect(draggable.draggedElm.translateY).toEqual(EXPECTED_TRANSLATE_Y);
-      // });
+    describe.skip("Stimulates mousemove - Checks dragAt()", () => {
+      beforeAll(() => {
+        for (let i = 0; i < MOVING_PIXELS_R2; i += 1) {
+          draggable.dragAt(START_CLIENT_X_R2 + i, START_CLIENT_Y_R2 + i);
+
+          EXPECTED_TRANSLATE_X_R2 =
+            START_CLIENT_X_R2 + i + EXPECTED_OFFSET_X_R2;
+          EXPECTED_TRANSLATE_Y_R2 =
+            START_CLIENT_Y_R2 + i + EXPECTED_OFFSET_Y_R2;
+        }
+      });
+
+      it("Offset never change", () => {
+        expect(draggable.offsetX).toEqual(EXPECTED_OFFSET_X_R2);
+        expect(draggable.offsetY).toEqual(EXPECTED_OFFSET_Y_R2);
+      });
+
+      it("Draggable updates translateX/Y in store", () => {
+        expect(draggable.draggedElm.translateX).toEqual(
+          EXPECTED_TRANSLATE_X_R2
+        );
+        expect(draggable.draggedElm.translateY).toEqual(
+          EXPECTED_TRANSLATE_Y_R2
+        );
+      });
     });
 
-    describe("Stimulates mouseup - Checks end()", () => {
-      // beforeAll(() => {
-      //   draggable.end();
-      // });
-      // it("Updates style to normal", () => {
-      //   expect(draggable.draggedStyle.pointerEvents).toBe("");
-      // });
+    describe.skip("Stimulates mouseup - Checks end()", () => {
+      beforeAll(() => {
+        draggable.end();
+      });
+
+      it("Updates style to normal", () => {
+        expect(draggable.draggedStyle.pointerEvents).toBe("");
+      });
     });
   });
 });
