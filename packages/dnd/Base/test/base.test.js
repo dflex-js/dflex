19a58328d361d01768cdcbb01344018e24ffcdcb
jalal246 <jimmy002020@gmail.com>
2020-06-29 19:34:49 +0200

    Add tests for basic flags

--- packages/dnd/Base/test/base.test.js
+++ packages/dnd/Base/test/base.test.js
@@ -1,11 +1,44 @@
+import createElement from "dflex-utils-test";
+
 import Base from "../src";
+import store from "../../Store";
+
+const childInstance1 = createElement();
+const childInstance2 = createElement();
+
+childInstance1.depth = 0;
+childInstance2.depth = 0;
+
+const parentInstance = createElement({
+  children: [childInstance1.element, childInstance2.element],
+});
+parentInstance.depth = 1;
+
+beforeAll(() => {
+  store.register(childInstance1);
+  store.register(childInstance2);
+
+  store.register(parentInstance);
+});
 
 let base;
 
 describe("DND - PKG: Base", () => {
   beforeAll(() => {
-    base = new Base();
+    const START_CLIENT_X = 10;
+    const START_CLIENT_Y = 20;
+
+    base = new Base(childInstance1.id, {
+      x: START_CLIENT_X,
+      y: START_CLIENT_Y,
+    });
   });
 
-  it("pass", () => {});
+  it("Checks node siblings", () => {
+    expect(base.isSingleton).toBe(false);
+  });
+
+  it("Checks node parent", () => {
+    expect(base.isOrphan).toBe(false);
+  });
 });
