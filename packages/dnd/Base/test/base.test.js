bd1ea2a930a572fd8a1f5213772839a419cbe191
jalal246 <jimmy002020@gmail.com>
2020-07-01 18:32:31 +0200

    Add test for parent contianer

--- packages/dnd/Base/test/base.test.js
+++ packages/dnd/Base/test/base.test.js
@@ -35,50 +35,88 @@ beforeAll(() => {
 let base;
 
 describe("DND - PKG: Base", () => {
-  beforeAll(() => {
-    const START_CLIENT_X = 10;
-    const START_CLIENT_Y = 20;
+  describe("Testing child", () => {
+    beforeAll(() => {
+      const START_CLIENT_X = 10;
+      const START_CLIENT_Y = 20;
 
-    base = new Base(childInstance1.id, {
-      x: START_CLIENT_X,
-      y: START_CLIENT_Y,
+      base = new Base(childInstance1.id, {
+        x: START_CLIENT_X,
+        y: START_CLIENT_Y,
+      });
+    });
+
+    describe("Testing flags and relations", () => {
+      it("Checks node siblings", () => {
+        expect(base.isSingleton).toBe(false);
+      });
+
+      it("Checks node parent", () => {
+        expect(base.isOrphan).toBe(false);
+      });
+
+      it("Checks parent list", () => {
+        expect(base.parentsList).toBe(parentInstance.id);
+      });
+
+      it("Checks siblings list", () => {
+        expect(base.siblingsList).toStrictEqual([
+          childInstance1.id,
+          childInstance2.id,
+        ]);
+      });
+
+      it("Detects activeParent", () => {
+        expect(base.activeParent.id).toBe(parentInstance.id);
+      });
+    });
+
+    describe("Threshold", () => {
+      it("Calculates threshold Offset", () => {
+        expect(base[DRAGGED_ELM].thresholdOffset).toMatchSnapshot();
+      });
+
+      it("Calculates thresholds for dragged as dropped", () => {
+        expect(base.thresholds.dragged).toMatchSnapshot();
+      });
+
+      it("Calculates thresholds for parent as dropped", () => {
+        expect(base.thresholds.parents).toMatchSnapshot();
+      });
     });
   });
 
-  it("Checks node siblings", () => {
-    expect(base.isSingleton).toBe(false);
-  });
+  describe("Testing parent", () => {
+    beforeAll(() => {
+      const START_CLIENT_X = 10;
+      const START_CLIENT_Y = 20;
 
-  it("Checks node parent", () => {
-    expect(base.isOrphan).toBe(false);
-  });
-
-  it("Checks parent list", () => {
-    expect(base.parentsList).toBe(parentInstance.id);
-  });
-
-  it("Checks siblings list", () => {
-    expect(base.siblingsList).toStrictEqual([
-      childInstance1.id,
-      childInstance2.id,
-    ]);
-  });
-
-  it("Detects activeParent", () => {
-    expect(base.activeParent.id).toBe(parentInstance.id);
-  });
-
-  describe("Threshold", () => {
-    it("Calculates threshold Offset", () => {
-      expect(base[DRAGGED_ELM].thresholdOffset).toMatchSnapshot();
+      base = new Base(parentInstance.id, {
+        x: START_CLIENT_X,
+        y: START_CLIENT_Y,
+      });
     });
 
-    it("Calculates thresholds for dragged as dropped", () => {
-      expect(base.thresholds.dragged).toMatchSnapshot();
-    });
+    describe("Testing flags and relations", () => {
+      it("Checks node siblings", () => {
+        expect(base.isSingleton).toBe(true);
+      });
 
-    it("Calculates thresholds for parent as dropped", () => {
-      expect(base.thresholds.parents).toMatchSnapshot();
+      it("Checks node parent", () => {
+        expect(base.isOrphan).toBe(true);
+      });
+
+      it("Checks parent list", () => {
+        expect(base.parentsList).toBe(undefined);
+      });
+
+      it("Checks siblings list", () => {
+        expect(base.siblingsList).toBe(parentInstance.id);
+      });
+
+      it("Detects activeParent", () => {
+        expect(base.activeParent).toBe(undefined);
+      });
     });
   });
 });
