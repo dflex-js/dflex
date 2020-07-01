6b375bf4ec6ed816de2f1eeab2a586ef6cb2951f
jalal246 <jimmy002020@gmail.com>
2020-07-01 20:48:21 +0200

    Add tests for last element and inner offset

--- packages/dnd/Draggable/test/draggable.test.js
+++ packages/dnd/Draggable/test/draggable.test.js
@@ -25,18 +25,68 @@ beforeAll(() => {
 
 let draggable;
 
+const START_CLIENT_X = 10;
+const START_CLIENT_Y = 20;
+
+let innerXOffset;
+let innerYOffset;
 describe("DND - PKG: Draggable", () => {
   beforeAll(() => {
-    const START_CLIENT_X = 10;
-    const START_CLIENT_Y = 20;
-
     draggable = new Draggable(childInstance1.id, {
       x: START_CLIENT_X,
       y: START_CLIENT_Y,
     });
   });
 
-  it("Check if dragged is last element in the list", () => {
+  it("Checks if dragged is last element in the list", () => {
     expect(draggable.isDraggedLastElm()).toEqual(false);
+  });
+
+  it("Current offset is equal to initial offset", () => {
+    expect(draggable[DRAGGED_ELM].offset.top).toEqual(
+      draggable[DRAGGED_ELM].currentTop
+    );
+    expect(draggable[DRAGGED_ELM].offset.left).toEqual(
+      draggable[DRAGGED_ELM].currentLeft
+    );
+  });
+
+  it("Calculated inner offset", () => {
+    innerXOffset = draggable.innerXOffset;
+    innerYOffset = draggable.innerYOffset;
+
+    expect(innerXOffset).toBeTruthy();
+    expect(innerYOffset).toBeTruthy();
+  });
+
+  it("Current offset isn't equal to initial offset after element dragged", () => {
+    const MOVING_PIXELS = 100;
+    for (let i = 0; i < MOVING_PIXELS; i += 1) {
+      draggable.dragAt(START_CLIENT_X + i, START_CLIENT_Y + i);
+    }
+
+    draggable.endDragging();
+
+    expect(draggable[DRAGGED_ELM].offset.top).not.toBe(
+      draggable[DRAGGED_ELM].currentTop
+    );
+    expect(draggable[DRAGGED_ELM].offset.left).not.toBe(
+      draggable[DRAGGED_ELM].currentLeft
+    );
+  });
+
+  it("Current offset is set correctly", () => {
+    expect(draggable[DRAGGED_ELM].currentTop).toEqual(
+      draggable[DRAGGED_ELM].offset.top + draggable[DRAGGED_ELM].translateX
+    );
+
+    expect(draggable[DRAGGED_ELM].currentLeft).toEqual(
+      draggable[DRAGGED_ELM].offset.left + draggable[DRAGGED_ELM].translateY
+    );
+  });
+
+  it("Makes sure inner offset not changed after drag", () => {
+    expect(draggable.innerXOffset).toEqual(innerXOffset);
+    expect(draggable.innerYOffset).toEqual(innerYOffset);
   });
 });
