b0a68c953f0957346fc135513ebafc5baf2f7cc6
jalal246 <jimmy002020@gmail.com>
2020-07-14 03:02:34 +0200

    Inits

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  projects: ["<rootDir>", "<rootDir>/packages/dnd"],
+
   testEnvironment: "jsdom",
+  preset: "jest-puppeteer",
+
   testPathIgnorePatterns: ["site"],
 };
---
+++ packages/dnd/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  displayName: "test",
+  preset: "jest-puppeteer",
+};
