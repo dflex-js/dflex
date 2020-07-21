888b09718430667c2b0dbdc4316997c1aa2f69bf
jalal246 <jimmy002020@gmail.com>
2020-07-22 01:32:18 +0200

    Fix first test casein cypress

--- cypress/integration/dnd/actions.spec.js
+++ cypress/integration/dnd/actions.spec.js
@@ -1,4 +1,7 @@
 // / <reference types="cypress" />
+let startingPointX;
+let startingPointY;
+let elmBox;
 
 context("Actions", () => {
   beforeEach(() => {
@@ -14,9 +17,21 @@ context("Actions", () => {
 
   it(".click() - click on a DOM element", () => {
     // Main button pressed (usually the left button)
-    cy.get("#id-10")
-      .trigger("mousedown", { button: 0 })
-      .trigger("mousemove", 0, 0, { force: true })
-      .trigger("mouseup", { force: true });
+
+    cy.get("#id-10").then((elm) => {
+      elmBox = elm[0].getBoundingClientRect();
+
+      startingPointX = elmBox.x + elmBox.width / 2;
+      startingPointY = elmBox.y + elmBox.height / 2;
+
+      cy.get("#id-10")
+        .trigger("mousedown", { button: 0 })
+        .trigger("mousemove", {
+          clientX: 700,
+          clientY: startingPointY,
+          force: true,
+        })
+        .trigger("mouseup", { force: true });
+    });
   });
 });
