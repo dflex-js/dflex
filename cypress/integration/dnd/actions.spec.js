6e060446dfb84f2f36baed4b964cfcbbd015db1c
jalal246 <jimmy002020@gmail.com>
2020-07-22 03:22:33 +0200

    Fiix test decriptions

--- cypress/integration/dnd/actions.spec.js
+++ cypress/integration/dnd/actions.spec.js
@@ -5,12 +5,12 @@ let startingPointY;
 let elmBox;
 
 context("DnD/Testing is out", () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit("http://localhost:3001/");
   });
 
-  context("Moving horizontally", () => {
-    it("Going out from the right", () => {
+  context("Moving horizontally- out form the right", () => {
+    it("Transforms element 10 out", () => {
       cy.get("#id-10").then((elm) => {
         elmBox = elm[0].getBoundingClientRect();
 
@@ -18,36 +18,42 @@ context("DnD/Testing is out", () => {
         startingPointY = elmBox.y + elmBox.height / 2;
 
         cy.get("#id-10")
-          .trigger("mousedown", { button: 0 })
+          .trigger("mousedown", {
+            button: 0,
+          })
           .trigger("mousemove", {
             clientX: startingPointX + ((2 / 3) * elmBox.width + 2),
             clientY: startingPointY,
             force: true,
           });
       });
+    });
 
+    it("Effects another element, lowers the up", () => {
       cy.get("#id-9").should(
         "have.css",
         "transform",
         "matrix(1, 0, 0, 1, 0, 58)"
       );
+    });
 
+    it("Does not effect element 11", () => {
       cy.get("#id-11").should("have.css", "transform", "none");
+    });
 
-      cy.get("#p0-1c")
-        .should("not.be.empty")
-        .and(($li) => {
-          expect($li[0].children).to.have.length(3);
-        });
-
+    it("Triggers mouseup", () => {
       cy.get("#id-10").trigger("mouseup", { force: true });
+    });
 
+    it("Makes sure list has three elements", () => {
       cy.get("#p0-1c")
         .should("not.be.empty")
         .and(($li) => {
           expect($li[0].children).to.have.length(3);
         });
+    });
 
+    it("Resets all positions", () => {
       cy.get("#id-9").should(
         "have.css",
         "transform",
