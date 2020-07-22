69beb45a5593532d2169867e691eaf85d6be17cb
jalal246 <jimmy002020@gmail.com>
2020-07-22 03:11:48 +0200

    Add first context

--- cypress/integration/dnd/actions.spec.js
+++ cypress/integration/dnd/actions.spec.js
@@ -1,37 +1,69 @@
 // / <reference types="cypress" />
+
 let startingPointX;
 let startingPointY;
 let elmBox;
 
-context("Actions", () => {
+context("DnD/Testing is out", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3001/");
   });
 
-  function movePiece(id, x, y) {
-    cy.get(id)
-      .trigger("mousedown", { which: 1 })
-      .trigger("mousemove", { clientX: x, clientY: y })
-      .trigger("mouseup", { force: true });
-  }
+  context("Moving horizontally", () => {
+    it("Going out from the right", () => {
+      cy.get("#id-10").then((elm) => {
+        elmBox = elm[0].getBoundingClientRect();
 
-  it(".click() - click on a DOM element", () => {
-    // Main button pressed (usually the left button)
+        startingPointX = elmBox.x + elmBox.width / 2;
+        startingPointY = elmBox.y + elmBox.height / 2;
 
-    cy.get("#id-10").then((elm) => {
-      elmBox = elm[0].getBoundingClientRect();
+        cy.get("#id-10")
+          .trigger("mousedown", { button: 0 })
+          .trigger("mousemove", {
+            clientX: startingPointX + ((2 / 3) * elmBox.width + 2),
+            clientY: startingPointY,
+            force: true,
+          });
+      });
 
-      startingPointX = elmBox.x + elmBox.width / 2;
-      startingPointY = elmBox.y + elmBox.height / 2;
+      cy.get("#id-9").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 58)"
+      );
 
-      cy.get("#id-10")
-        .trigger("mousedown", { button: 0 })
-        .trigger("mousemove", {
-          clientX: 700,
-          clientY: startingPointY,
-          force: true,
-        })
-        .trigger("mouseup", { force: true });
+      cy.get("#id-11").should("have.css", "transform", "none");
+
+      cy.get("#p0-1c")
+        .should("not.be.empty")
+        .and(($li) => {
+          expect($li[0].children).to.have.length(3);
+        });
+
+      cy.get("#id-10").trigger("mouseup", { force: true });
+
+      cy.get("#p0-1c")
+        .should("not.be.empty")
+        .and(($li) => {
+          expect($li[0].children).to.have.length(3);
+        });
+
+      cy.get("#id-9").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+      cy.get("#id-10").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+
+      cy.get("#id-11").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
     });
   });
 });
