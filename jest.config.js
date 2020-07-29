ae9334fc660c1ee22dce109c4223e3c0d82d7ea8
jalal246 <jimmy002020@gmail.com>
2020-07-29 22:11:29 +0200

    fix unit test and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["e2e-test", "site", "test/integration"],
+  testPathIgnorePatterns: ["cypress", "site", "test/integration"],
 };
