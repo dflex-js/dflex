102e2bce4049c6df9123c2a3413162568a843577
jalal246 <jimmy002020@gmail.com>
2020-08-03 18:15:59 +0200

    still the same error (eslint in github ci acts differently from local)

--- packages/dnd/cypress/.eslintrc.js
+++ packages/dnd/cypress/.eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   extends: ["plugin:cypress/recommended"],
   rules: {
-    "import/no-extraneous-dependencies": [
-      "error",
-      { devDependencies: ["./plugin/", "./support/"] },
-    ],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
   },
 };
