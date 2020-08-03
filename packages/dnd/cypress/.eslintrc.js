9eaa98972bb7ef47d7ea32532fee11a1861622bc
jalal246 <jimmy002020@gmail.com>
2020-08-03 18:11:02 +0200

    WIP

--- packages/dnd/cypress/.eslintrc.js
+++ packages/dnd/cypress/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     "import/no-extraneous-dependencies": [
       "error",
-      { devDependencies: ["./cypress/"] },
+      { devDependencies: ["./plugin/", "./support/"] },
     ],
   },
 };
