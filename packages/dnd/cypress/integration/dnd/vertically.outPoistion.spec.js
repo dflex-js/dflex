191bfdc4787cba6c1e59de1bf88ef38ecc9f08c6
jalal246 <jimmy002020@gmail.com>
2020-08-03 23:10:23 +0200

    Add new test case when pass more than one sibling

--- packages/dnd/cypress/integration/dnd/vertically.outPoistion.spec.js
+++ packages/dnd/cypress/integration/dnd/vertically.outPoistion.spec.js
@@ -160,4 +160,102 @@ context("DnD/Testing is out position vertically", () => {
       );
     });
   });
+
+  context("Moving strict vertically multi siblings - out down", () => {
+    it("Transforms element 10 out", () => {
+      console.log("===============");
+      cy.get("#id-10").then((elm) => {
+        elmBox = elm[0].getBoundingClientRect();
+
+        startingPointX = elmBox.x + elmBox.width / 2;
+        startingPointY = elmBox.y + elmBox.height / 2;
+
+        cy.get("#id-10")
+          .trigger("mousedown", {
+            button: 0,
+          })
+          .trigger("mousemove", {
+            clientX: startingPointX,
+            clientY: startingPointY + ((2 / 3) * elmBox.height + 2),
+            force: true,
+          })
+          .trigger("mousemove", {
+            clientX: startingPointX,
+            clientY:
+              startingPointY + (2 * elmBox.height + (1 / 3) * elmBox.height),
+            force: true,
+          });
+      });
+    });
+
+    it("Does not effect element 9", () => {
+      cy.get("#id-9").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+    });
+
+    it("Effects element 11, lifts it up", () => {
+      cy.get("#id-11").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, -58)"
+      );
+    });
+
+    it("Effects element 12, lifts it up", () => {
+      cy.get("#id-12").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, -58)"
+      );
+    });
+
+    it.skip("Moves outside the parent", () => {
+      cy.get("#id-10").trigger("mousemove", {
+        clientX: startingPointX,
+        clientY: startingPointY + (2 * elmBox.height + 2 * elmBox.height),
+        force: true,
+      });
+    });
+
+    it.skip("Triggers mouseup", () => {
+      cy.get("#id-10").trigger("mouseup", { force: true });
+    });
+
+    it.skip("Makes sure list has four elements", () => {
+      cy.get("#p0-1c")
+        .should("not.be.empty")
+        .and(($li) => {
+          expect($li[0].children).to.have.length(4);
+        });
+    });
+
+    it.skip("Resets all positions", () => {
+      cy.get("#id-9").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+
+      cy.get("#id-10").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+
+      cy.get("#id-11").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+
+      cy.get("#id-12").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+    });
+  });
 });
