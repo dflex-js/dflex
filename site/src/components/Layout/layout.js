c8f2efea37ed4646aa5edb465973adb32f87b44e
jalal246 <jimmy002020@gmail.com>
2020-09-03 19:53:07 +0200

    Add fake footer

--- site/src/components/Layout/layout.js
+++ site/src/components/Layout/layout.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 import Header from "../LayoutHeader";
-import Main from "../MainLayout";
+import Footer from "../LayoutFooter";
+
+// import Main from "../MainLayout";
+
+import Flex from "../Flex";
 
 import "./layout.css";
+import { media } from "../../theme";
 
 const Layout = ({ children }) => {
   return (
@@ -15,7 +20,26 @@ const Layout = ({ children }) => {
       }}
     >
       <Header />
-      <Main>{children}</Main>
+      <Flex
+        direction="column"
+        justifyContent="stretch"
+        css={{
+          flex: "1 0 auto",
+
+          marginTop: 60,
+
+          [media.between("medium", "large")]: {
+            marginTop: 50,
+          },
+
+          [media.lessThan("medium")]: {
+            marginTop: 40,
+          },
+        }}
+      >
+        {children}
+      </Flex>
+      <Footer />
     </div>
   );
 };
---
+++ site/src/components/LayoutFooter/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer({ children }) {
+  return <div>{children}</div>;
+}
+
+export default Footer;
