b806543a53e147a27d90171d83b959397f472d46
jalal246 <jimmy002020@gmail.com>
2020-09-03 17:07:14 +0200

    Add shared config

---
+++ site/config.js
@@ -0,0 +1,12 @@
+const config = {
+  siteMetadata: {
+    title: `DFlex`,
+    description: "A JavaScript ambitious project to manipulate DOM elements",
+    docsLocation: null,
+    author: `Jalal Maskoun`,
+    ogImage: null,
+    favicon: null,
+  },
+};
+
+module.exports = config;
