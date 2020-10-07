cb15385b831f96e70c78ec94bf4e04d1fafa5014
jalal246 <jimmy002020@gmail.com>
2020-10-07 23:03:38 +0200

    Update site id

--- site/config.js
+++ site/config.js
@@ -2,7 +2,7 @@ const config = {
   siteUrl: "https://dflex.netlify.app",
   title: "DFlex",
   description: "A JavaScript Ambitious Project to Manipulate DOM Elements",
-  trackingId: "UA-167775444-1",
+  trackingId: "G-6BBMT8R70B",
   author: {
     name: "Jalal Maskoun",
   },
