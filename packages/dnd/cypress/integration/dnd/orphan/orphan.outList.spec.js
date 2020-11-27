2154fe0846c4287c2c7c560b0d457a5842efe10c
jalal246 <jimmy002020@gmail.com>
2020-11-27 20:13:15 +0200

    Add new test cases

--- packages/dnd/cypress/integration/dnd/orphan/orphan.outList.spec.js
+++ packages/dnd/cypress/integration/dnd/orphan/orphan.outList.spec.js
@@ -1,11 +1,84 @@
+let startingPointX;
+let startingPointY;
+let elmBox;
+
+let stepX;
+let stepY;
+
+function backToOriginalPosition() {
+  cy.get("#id-1").trigger("mousemove", {
+    clientX: startingPointX,
+    clientY: startingPointY,
+    force: true,
+  });
+}
+
 context("DnD/Testing single element is the list", () => {
   before(() => {
     cy.visit("http://localhost:3001/");
   });
 
-  context("Moving element outside the list from the bottom", () => {});
-  context("Moving element outside the list from the top", () => {});
-  context("Moving element outside the list from the right", () => {});
-  context("Moving element outside the list from the left", () => {});
-  context("End transformation", () => {});
+  context("Moving element outside the list horizontally", () => {
+    it("Clicks on (container1 |> elm-1) ", () => {
+      cy.get("#id-1").then((elm) => {
+        elmBox = elm[0].getBoundingClientRect();
+        startingPointX = elmBox.x + elmBox.width / 2;
+        startingPointY = elmBox.y + elmBox.height / 2;
+
+        stepX = (2 / 3) * elmBox.width + 2;
+        stepY = (2 / 3) * elmBox.height + 2;
+      });
+
+      cy.get("#id-1").trigger("mousedown", {
+        button: 0,
+      });
+    });
+
+    it("Moves element out list to the right", () => {
+      cy.get("#id-1").trigger("mousemove", {
+        clientX: startingPointX + stepX,
+        clientY: startingPointY,
+        force: true,
+      });
+    });
+
+    it("Moves element out list to the left", () => {
+      backToOriginalPosition();
+
+      cy.get("#id-1").trigger("mousemove", {
+        clientX: startingPointX - stepX,
+        clientY: startingPointY,
+        force: true,
+      });
+
+      backToOriginalPosition();
+    });
+  });
+
+  context("Moving element outside the list vertically", () => {
+    it("Moves element out list to the top", () => {
+      cy.get("#id-1").trigger("mousemove", {
+        clientX: startingPointX,
+        clientY: startingPointY - stepY,
+        force: true,
+      });
+
+      backToOriginalPosition();
+    });
+
+    it("Moves element out list to the top", () => {
+      cy.get("#id-1").trigger("mousemove", {
+        clientX: startingPointX,
+        clientY: startingPointY + stepY,
+        force: true,
+      });
+
+      backToOriginalPosition();
+    });
+  });
+  context("End transformation", () => {
+    it("Triggers mouseup safely without errors", () => {
+      cy.get("#id-1").trigger("mouseup", { force: true });
+    });
+  });
 });
