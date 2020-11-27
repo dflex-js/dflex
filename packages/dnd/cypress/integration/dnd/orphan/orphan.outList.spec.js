cd85c8da7dd86fee72481da75dc9796f57db5f57
jalal246 <jimmy002020@gmail.com>
2020-11-27 19:44:35 +0200

    init test for orphan

---
+++ packages/dnd/cypress/integration/dnd/orphan/orphan.outList.spec.js
@@ -0,0 +1,11 @@
+context("DnD/Testing single element is the list", () => {
+  before(() => {
+    cy.visit("http://localhost:3001/");
+  });
+
+  context("Moving element outside the list from the bottom", () => {});
+  context("Moving element outside the list from the top", () => {});
+  context("Moving element outside the list from the right", () => {});
+  context("Moving element outside the list from the left", () => {});
+  context("End transformation", () => {});
+});
