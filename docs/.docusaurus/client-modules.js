77b4440732732b20ddc8021bc8a2c88f6366d0af
jalal246 <jimmy002020@gmail.com>
2021-01-20 22:31:46 +0200

    remove .docusaurus

--- docs/.docusaurus/client-modules.js
+++
@@ -1,9 +0,0 @@
-export default [
-  require("D:\\projects\\dflex\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("D:\\projects\\dflex\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("D:\\projects\\dflex\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("D:\\projects\\dflex\\node_modules\\@docusaurus\\plugin-google-gtag\\src\\gtag"),
-  require("D:\\projects\\dflex\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\projects\\dflex\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\projects\\dflex\\docs\\src\\css\\custom.css"),
-];
