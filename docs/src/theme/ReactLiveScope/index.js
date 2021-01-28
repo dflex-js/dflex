edcb8fef9841e12fd89a558315a3604eeca73d3e
jalal246 <jimmy002020@gmail.com>
2021-01-28 09:48:49 +0200

    Remove unnecessary components

--- docs/src/theme/ReactLiveScope/index.js
+++ docs/src/theme/ReactLiveScope/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import DFlexDraggable from "@dflex/draggable";
 import DFlexDnD from "@dflex/dnd";
 
-import Todo from "./todo";
-
 // Draggable live
 import "./draggable.css";
 import HandlerSVG from "./HandlerSVG";
