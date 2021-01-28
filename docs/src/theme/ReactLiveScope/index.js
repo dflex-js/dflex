8e5ac1c0d8a77a94724a734e23a9f563144a7a46
jalal246 <jimmy002020@gmail.com>
2021-01-28 02:03:39 +0200

    add todo example to the siite

--- docs/src/theme/ReactLiveScope/index.js
+++ docs/src/theme/ReactLiveScope/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import DFlexDraggable from "@dflex/draggable";
+import DFlexDnD from "@dflex/dnd";
 
-import "./index.css";
+// Draggable live
+import "./draggable.css";
 import HandlerSVG from "./HandlerSVG";
+
+// DnD live
+import "./todo.css";
 
 const ReactLiveScope = {
   React,
   ...React,
   HandlerSVG,
   DFlexDraggable,
+  DFlexDnD,
 };
 
 export default ReactLiveScope;
