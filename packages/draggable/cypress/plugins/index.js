145fc054af8ead7d9644a2c0322066ab5aa3f35d
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2021-02-16 14:50:21 +0530

    eslint failure check

--- packages/draggable/cypress/plugins/index.js
+++ packages/draggable/cypress/plugins/index.js
@@ -18,4 +18,7 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+  // eslint-disable-next-line global-require
+  require("@cypress/code-coverage/task")(on, config);
+  return config;
+};
