8103011fe0bda6b0e77e9af627d221c2638fcb68
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2021-02-16 09:07:05 +0530

    added cypress to packages/draggable
    added basic configuration of cypress
    
    integration specs passed

---
+++ packages/draggable/cypress/integration/sample_spec.js
@@ -0,0 +1,8 @@
+describe("checking", function () {
+  //   it("returns noice", function () {
+  //     expect(true).to.equal(true);
+  //   });
+  it("actions", function () {
+    cy.visit("https://jalal246.github.io/dflex/");
+  });
+});
