f95e7fd7c79739e794b2f6e9ef9a298c7547a6b3
jalal246 <jimmy002020@gmail.com>
2021-03-12 06:24:05 +0200

    fix lock

--- packages/draggable/cypress/integration/TODO.js
+++ packages/draggable/cypress/integration/TODO.js
@@ -1,7 +1,6 @@
+// TODO
+
 describe("checking", () => {
-  //   it("returns noice", function () {
-  //     expect(true).to.equal(true);
-  //   });
   it("actions", () => {
     cy.visit("https://jalal246.github.io/dflex/");
   });
