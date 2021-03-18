fb45115da72fdc93f5cc3084b0b5d42b1faf2f7a
jalal246 <jimmy002020@gmail.com>
2021-03-18 07:13:34 +0200

    add tests

--- packages/dnd/cypress/integration/dnd/outPosition/generic.outPoistion.spec.js
+++ packages/dnd/cypress/integration/dnd/outPosition/generic.outPoistion.spec.js
@@ -1,151 +1,147 @@
-// let elmBox;
-// let startingPointX;
-// let startingPointY;
+let elmBox;
+let startingPointX;
+let startingPointY;
 
-// let steps;
+let steps;
 
-// context("Moving and returning from the same position", () => {
-//   before(() => {
-//     cy.visit("http://localhost:3001");
-//   });
+const waitingTime = 6;
 
-//   it("Transforms (container3 |> elm-1) out", () => {
-//     cy.get("#id-9").then((elm) => {
-//       elmBox = elm[0].getBoundingClientRect();
-//       startingPointX = elmBox.x + elmBox.width / 2;
-//       startingPointY = elmBox.y + elmBox.height / 2;
+context("Moving and returning from the same position", () => {
+  before(() => {
+    cy.visit("http://localhost:3001");
+  });
 
-//       cy.get("#id-9")
-//         .trigger("mousedown", {
-//           button: 0,
-//         })
-//         .trigger("mousemove", {
-//           clientX: startingPointX - (elmBox.width + 2),
-//           clientY: startingPointY - (elmBox.height + 2),
-//           force: true,
-//         });
-//     });
-//   });
+  it("Transforms (container3 |> elm-1) out", () => {
+    cy.get("#id-9").then((elm) => {
+      elmBox = elm[0].getBoundingClientRect();
+      startingPointX = elmBox.x + elmBox.width / 2;
+      startingPointY = elmBox.y + elmBox.height / 2;
 
-//   it("Transforms (container3 |> elm-1) in to the original Y-position", () => {
-//     for (let i = 0; i < elmBox.height + 2; i += 1) {
-//       cy.get("#id-9").trigger("mousemove", {
-//         clientY: startingPointY - (elmBox.height + 2) + i,
-//         force: true,
-//       });
-//       // eslint-disable-next-line cypress/no-unnecessary-waiting
-//       cy.wait(6);
-//     }
-//   });
+      cy.get("#id-9")
+        .trigger("mousedown", {
+          button: 0,
+        })
+        .trigger("mousemove", {
+          clientX: startingPointX - (elmBox.width + 2),
+          clientY: startingPointY - (elmBox.height + 2),
+          force: true,
+        });
+    });
+  });
 
-//   it("Transforms (container3 |> elm-1) in to the original X-position", () => {
-//     for (let i = 0; i < elmBox.width + 2; i += 1) {
-//       cy.get("#id-9").trigger("mousemove", {
-//         clientX: startingPointX - (elmBox.width + 2) + i,
-//         force: true,
-//       });
-//       // eslint-disable-next-line cypress/no-unnecessary-waiting
-//       cy.wait(6);
-//     }
-//   });
+  it("Transforms (container3 |> elm-1) in to the original Y-position", () => {
+    for (let i = 0; i < elmBox.height + 2; i += 1) {
+      cy.get("#id-9").trigger("mousemove", {
+        clientY: startingPointY - (elmBox.height + 2) + i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(waitingTime);
+    }
+  });
 
-//   it("Triggers mouseup", () => {
-//     cy.get("#id-9").trigger("mouseup", { force: true });
-//   });
+  it("Transforms (container3 |> elm-1) in to the original X-position", () => {
+    for (let i = 0; i < elmBox.width + 2; i += 1) {
+      cy.get("#id-9").trigger("mousemove", {
+        clientX: startingPointX - (elmBox.width + 2) + i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(waitingTime);
+    }
+  });
 
-//   it("All list holds the right position", () => {
-//     cy.get("#id-9").should("have.css", "transform", "matrix(1, 0, 0, 1, 0, 0)");
+  it("Triggers mouseup", () => {
+    cy.get("#id-9").trigger("mouseup", { force: true });
+  });
 
-//     cy.get("#id-10").should(
-//       "have.css",
-//       "transform",
-//       "matrix(1, 0, 0, 1, 0, 0)"
-//     );
+  it("All list holds the right position", () => {
+    cy.get("#id-9").should("have.css", "transform", "matrix(1, 0, 0, 1, 0, 0)");
 
-//     cy.get("#id-11").should(
-//       "have.css",
-//       "transform",
-//       "matrix(1, 0, 0, 1, 0, 0)"
-//     );
+    cy.get("#id-10").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 0)"
+    );
 
-//     cy.get("#id-12").should(
-//       "have.css",
-//       "transform",
-//       "matrix(1, 0, 0, 1, 0, 0)"
-//     );
-//   });
-// });
+    cy.get("#id-11").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 0)"
+    );
 
-// context("Swinging", () => {
-//   before(() => {
-//     cy.visit("http://localhost:3001");
-//   });
+    cy.get("#id-12").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 0)"
+    );
+  });
+});
 
-//   it("Transforms (container3 |> elm-1) out", () => {
-//     cy.get("#id-9").then((elm) => {
-//       elmBox = elm[0].getBoundingClientRect();
-//       startingPointX = elmBox.x + elmBox.width / 2;
-//       startingPointY = elmBox.y + elmBox.height / 2;
+context("Swinging - Confusion", () => {
+  before(() => {
+    cy.visit("http://localhost:3001");
+  });
 
-//       cy.get("#id-9")
-//         .trigger("mousedown", {
-//           button: 0,
-//         })
-//         .trigger("mousemove", {
-//           clientX: startingPointX - (elmBox.width + 2),
-//           clientY: startingPointY - (elmBox.height + 2),
-//           force: true,
-//         });
-//     });
-//   });
+  it("Transforms (container3 |> elm-1) out", () => {
+    cy.get("#id-9").then((elm) => {
+      elmBox = elm[0].getBoundingClientRect();
+      startingPointX = elmBox.x + elmBox.width / 2;
+      startingPointY = elmBox.y + elmBox.height / 2;
 
-//   it("Transforms (container3 |> elm-1) in to the original Y-position", () => {
-//     steps = elmBox.height + 2 + 22;
+      cy.get("#id-9")
+        .trigger("mousedown", {
+          button: 0,
+        })
+        .trigger("mousemove", {
+          clientX: startingPointX - (elmBox.width + 2),
+          clientY: startingPointY - (elmBox.height + 2),
+          force: true,
+        });
+    });
+  });
 
-//     for (let i = 0; i < steps; i += 1) {
-//       cy.get("#id-9").trigger("mousemove", {
-//         clientX: startingPointX - (elmBox.width - i),
-//         clientY: startingPointY - (elmBox.height + 2) + i,
-//         force: true,
-//       });
-//       // eslint-disable-next-line cypress/no-unnecessary-waiting
-//       cy.wait(6);
-//     }
-//   });
+  it("Transforms (container3 |> elm-1) in threshold confusion", () => {
+    steps = elmBox.height + 2 + 180;
 
-//   it("Checking siblings position", () => {
-//     cy.get("#id-10").should(
-//       "have.css",
-//       "transform",
-//       "matrix(1, 0, 0, 1, 0, -58)"
-//     );
+    for (let i = 0; i < steps; i += 1) {
+      cy.get("#id-9").trigger("mousemove", {
+        clientX: startingPointX - (elmBox.width - i),
+        clientY: startingPointY - (elmBox.height + 2) + i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(waitingTime);
+    }
+  });
 
-//     cy.get("#id-11").should(
-//       "have.css",
-//       "transform",
-//       "matrix(1, 0, 0, 1, 0, 0)"
-//     );
+  it("Triggers mouseup", () => {
+    cy.get("#id-9").trigger("mouseup", { force: true });
+  });
 
-//     cy.get("#id-12").should(
-//       "have.css",
-//       "transform",
-//       "matrix(1, 0, 0, 1, 0, 0)"
-//     );
-//   });
+  it("Checking new positions", () => {
+    cy.get("#id-9").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 174)"
+    );
 
-//   it("Moving more", () => {
-//     steps = elmBox.height + 2 + 22;
+    cy.get("#id-10").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, -58)"
+    );
 
-//     const moreSteps = steps + 12;
+    cy.get("#id-11").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, -58)"
+    );
 
-//     for (let i = steps; i < moreSteps; i += 1) {
-//       cy.get("#id-9").trigger("mousemove", {
-//         clientX: startingPointX - (elmBox.width - i),
-//         clientY: startingPointY - (elmBox.height + 2) + i,
-//         force: true,
-//       });
-//       // eslint-disable-next-line cypress/no-unnecessary-waiting
-//       cy.wait(6);
-//     }
-//   });
-// });
+    cy.get("#id-12").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, -58)"
+    );
+  });
+});
