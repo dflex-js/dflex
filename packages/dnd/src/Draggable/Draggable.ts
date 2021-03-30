7b6630290715afdd6a7800eba57a2f89c89aa2bb
jalal246 <jimmy002020@gmail.com>
2021-03-31 00:47:12 +0200

    Refactor boundries name

--- packages/dnd/src/Draggable/Draggable.ts
+++ packages/dnd/src/Draggable/Draggable.ts
@@ -93,7 +93,7 @@ class Draggable extends Base implements DraggableDnDInterface {
   }
 
   private selfHorizontalAxesFilter(x: number) {
-    const { left } = store.boundaries[
+    const { left } = store.siblingsBoundaries[
       store.registry[this.draggedElm.id].keys.sK
     ];
 
@@ -114,7 +114,7 @@ class Draggable extends Base implements DraggableDnDInterface {
   }
 
   private containerVerticalAxesFilter(y: number) {
-    const { maxTop, minTop } = store.boundaries[
+    const { maxTop, minTop } = store.siblingsBoundaries[
       store.registry[this.draggedElm.id].keys.sK
     ];
 
