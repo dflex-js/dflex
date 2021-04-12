b3cbf9155b4a29ba3bce02ebd94acd38ba0c4e6c
jalal246 <jimmy002020@gmail.com>
2021-04-12 03:13:24 +0200

    Remove coverage

--- packages/dnd/cypress/plugins/index.js
+++ packages/dnd/cypress/plugins/index.js
@@ -18,7 +18,7 @@
  */
 module.exports = (on, config) => {
   // eslint-disable-next-line global-require
-  require("@cypress/code-coverage/task")(on, config);
+  // require("@cypress/code-coverage/task")(on, config);
 
   // add other tasks to be registered here
 
