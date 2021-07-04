41990aa9d96a364629c3dc39edbb54cf6029b37e
jalal246 <jimmy002020@gmail.com>
2021-07-04 11:37:23 +0200

    wip

--- packages/dnd/playgrounds/dflex-react-dnd/src/App.tsx
+++ packages/dnd/playgrounds/dflex-react-dnd/src/App.tsx
@@ -7,7 +7,6 @@
 
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Basic from "./horizontal/list/Basic";
 
 import { ComponentBasedEvent, ContainerBasedEvent } from "./lists";
 import Restricted from "./restrictions";
@@ -17,9 +16,6 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/h-basic">
-          <Basic />
-        </Route>
         <Route path="/restricted">
           <Restricted />
         </Route>
