62ac1ef7405844ce74411bf5983dbbb1b6dde780
jalal246 <jimmy002020@gmail.com>
2021-07-17 14:24:26 +0200

    Use a better approach to verify

--- packages/dnd/src/Droppable/EndDroppable.ts
+++ packages/dnd/src/Droppable/EndDroppable.ts
@@ -75,7 +75,7 @@ class EndDroppable extends Droppable {
    * Undo list elements order and instances including translateX/Y and indexes
    * locally.
    */
-  private undoList(lst: Array<string>) {
+  private undoList(lst: string[]) {
     const {
       order: { self: from },
       id: draggedID,
@@ -96,20 +96,26 @@ class EndDroppable extends Droppable {
   }
 
   private verify(lst: string[]) {
-    for (let i = 0; i < lst.length; i += 1) {
-      const elmID = lst[i];
+    const siblingsBoundaries =
+      store.siblingsBoundaries[
+        store.registry[this.draggable.draggedElm.id].keys.sK
+      ];
 
-      if (this.isIDEligible2Move(elmID)) {
-        const element = store.getElmById(elmID);
+    const id = lst[0];
 
-        return (
-          Math.floor(element.currentTop) ===
-          Math.floor(element.ref.getBoundingClientRect().top)
-        );
-      }
+    if (id.length === 0 || this.draggable.draggedElm.id === id) {
+      return (
+        Math.floor(siblingsBoundaries.top) ===
+        Math.floor(this.draggable.occupiedOffset.currentTop)
+      );
     }
 
-    return false;
+    const element = store.getElmById(id);
+
+    return (
+      Math.floor(siblingsBoundaries.top) ===
+      Math.floor(element.ref.getBoundingClientRect().top)
+    );
   }
 
   endDragging() {
@@ -120,6 +126,7 @@ class EndDroppable extends Droppable {
     if (Array.isArray(siblings)) {
       if (this.draggable.isNotSettled() || !this.verify(siblings)) {
         isFallback = true;
+
         this.undoList(siblings);
       }
     }
