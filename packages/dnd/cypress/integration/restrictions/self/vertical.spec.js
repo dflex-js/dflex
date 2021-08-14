c6f31737411f77f4b11965e272f9fa5971d8676f
jalal246 <jimmy002020@gmail.com>
2021-08-14 15:23:37 +0200

    adding last changes

--- packages/dnd/cypress/integration/restrictions/self/vertical.spec.js
+++ packages/dnd/cypress/integration/restrictions/self/vertical.spec.js
@@ -36,7 +36,7 @@ context(
             force: true,
           });
           // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
+          // cy.wait(0);
         }
       });
 
@@ -111,7 +111,7 @@ context(
             force: true,
           });
           // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
+          // cy.wait(0);
         }
       });
 
@@ -186,7 +186,7 @@ context(
             force: true,
           });
           // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
+          // cy.wait(0);
         }
       });
 
@@ -249,7 +249,7 @@ context(
             force: true,
           });
           // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
+          // cy.wait(0);
         }
       });
 
@@ -320,7 +320,7 @@ context(
             force: true,
           });
           // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
+          // cy.wait(0);
         }
       });
 
@@ -401,7 +401,7 @@ context(
             force: true,
           });
           // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
+          // cy.wait(0);
         }
       });
 
@@ -471,96 +471,157 @@ context(
         );
       });
 
-      // it("Getting element again, transform it to the bottom", () => {
-      //   cy.get("#item-rest-bottom").trigger("mousedown", {
-      //     button: 0,
-      //   });
+      it("Getting element again, transform it to the bottom", () => {
+        cy.get("#item-rest-bottom").trigger("mousedown", {
+          button: 0,
+        });
 
-      //   for (let i = 0; i <= 300; i += 1) {
-      //     cy.get("#item-rest-bottom").trigger("mousemove", {
-      //       clientY: startingPointY + i,
-      //       force: true,
-      //     });
-      //     // eslint-disable-next-line cypress/no-unnecessary-waiting
-      //     cy.wait(0);
-      //   }
-      // });
+        for (let i = 0; i <= 10; i += 1) {
+          cy.get("#item-rest-bottom").trigger("mousemove", {
+            clientY: startingPointY + i,
+            force: true,
+          });
+          // eslint-disable-next-line cypress/no-unnecessary-waiting
+          // cy.wait(0);
+        }
+      });
 
-      // it("Dragged is inside the list", () => {
-      //   cy.get("#item-rest-bottom").should(
-      //     "have.css",
-      //     "transform",
-      //     "matrix(1, 0, 0, 1, 0, 0)"
-      //   );
-      // });
+      it("Dragged is inside the list", () => {
+        cy.get("#item-rest-bottom").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -83.1875)"
+        );
+      });
 
-      // it("Triggers mouseup", () => {
-      //   cy.get("#item-rest-bottom").trigger("mouseup", {
-      //     force: true,
-      //   });
-      // });
+      it("Triggers mouseup", () => {
+        cy.get("#item-rest-bottom").trigger("mouseup", {
+          force: true,
+        });
+      });
 
-      //   it("Getting element again, transform it to the top", () => {
-      //     cy.get("#item-rest-bottom").trigger("mousedown", {
-      //       button: 0,
-      //     });
+      it("Getting element again, transform it to the top", () => {
+        cy.get("#item-rest-bottom").trigger("mousedown", {
+          button: 0,
+        });
 
-      //     for (let i = 0; i <= 300; i += 10) {
-      //       cy.get("#item-rest-bottom").trigger("mousemove", {
-      //         clientY: startingPointY - i,
-      //         force: true,
-      //       });
-      //       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      //       cy.wait(0);
-      //     }
-      //   });
+        for (let i = 0; i <= 300; i += 10) {
+          cy.get("#item-rest-bottom").trigger("mousemove", {
+            clientY: startingPointY - i,
+            force: true,
+          });
+          // eslint-disable-next-line cypress/no-unnecessary-waiting
+          // cy.wait(0);
+        }
+      });
 
-      //   it("Siblings are lifted up", () => {
-      //     cy.get("#item-rest-left").should(
-      //       "have.css",
-      //       "transform",
-      //       "matrix(1, 0, 0, 1, 0, 0)"
-      //     );
+      it("Siblings are lifted up", () => {
+        cy.get("#item-rest-left").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 0)"
+        );
 
-      //     cy.get("#item-rest-right").should(
-      //       "have.css",
-      //       "transform",
-      //       "matrix(1, 0, 0, 1, 0, 0)"
-      //     );
+        cy.get("#item-rest-right").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 0)"
+        );
 
-      //     cy.get("#item-rest-left-right").should(
-      //       "have.css",
-      //       "transform",
-      //       "matrix(1, 0, 0, 1, 0, 0)"
-      //     );
+        cy.get("#item-rest-left-right").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 0)"
+        );
 
-      //     cy.get("#item-rest-top").should(
-      //       "have.css",
-      //       "transform",
-      //       "matrix(1, 0, 0, 1, 0, 166.375)"
-      //     );
+        cy.get("#item-rest-top").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 166.375)"
+        );
 
-      //     cy.get("#item-rest-top-bottom").should(
-      //       "have.css",
-      //       "transform",
-      //       "matrix(1, 0, 0, 1, 0, -174.375)"
-      //     );
+        cy.get("#item-rest-top-bottom").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -174.375)"
+        );
 
-      //     cy.get("#item-rest-all").should(
-      //       "have.css",
-      //       "transform",
-      //       "matrix(1, 0, 0, 1, 0, -174.375)"
-      //     );
-      //   });
+        cy.get("#item-rest-all").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -174.375)"
+        );
+      });
 
-      //   it("Triggers mouseup", () => {
-      //     cy.get("#item-rest-bottom").trigger("mouseup", {
-      //       force: true,
-      //     });
-      //   });
+      it("Getting element inside the list", () => {
+        for (let i = 300; i >= 250; i -= 10) {
+          cy.get("#item-rest-bottom").trigger("mousemove", {
+            clientY: startingPointY - i,
+            force: true,
+          });
+          // eslint-disable-next-line cypress/no-unnecessary-waiting
+          // cy.wait(0);
+        }
+      });
+
+      it("Triggers mouseup", () => {
+        cy.get("#item-rest-bottom").trigger("mouseup", {
+          force: true,
+        });
+      });
+
+      it("Siblings & dragged have new positions", () => {
+        cy.get("#item-rest-bottom").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -340.75)"
+        );
+
+        cy.get("#item-rest-left").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 91.1875)"
+        );
+
+        cy.get("#item-rest-right").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 91.1875)"
+        );
+
+        cy.get("#item-rest-left-right").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 91.1875)"
+        );
+
+        cy.get("#item-rest-top").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 257.562)"
+        );
+
+        cy.get("#item-rest-bottom").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -340.75)"
+        );
+
+        cy.get("#item-rest-top-bottom").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -83.1875)"
+        );
+
+        cy.get("#item-rest-all").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -83.1875)"
+        );
+      });
     });
 
-    context.skip("Top & bottom", () => {
+    context("Top & bottom", () => {
       it("Getting element (#item-rest-top-bottom)", () => {
         cy.get("#item-rest-top-bottom").then((elm) => {
           elmBox = elm[0].getBoundingClientRect();
@@ -573,16 +634,65 @@ context(
         });
       });
 
-      // it("Transforms (#item-rest-top-bottom) to the left", () => {
-      //   for (let i = 0; i <= 350; i += 1) {
-      //     cy.get("#item-rest-top-bottom").trigger("mousemove", {
-      //       clientX: startingPointX - i,
-      //       force: true,
-      //     });
-      //     // eslint-disable-next-line cypress/no-unnecessary-waiting
-      //     cy.wait(10);
-      //   }
-      // });
+      it("Transforms (#item-rest-top-bottom) to the left", () => {
+        for (let i = 0; i <= 350; i += 10) {
+          cy.get("#item-rest-top-bottom").trigger("mousemove", {
+            clientX: startingPointX - i,
+            force: true,
+          });
+          // eslint-disable-next-line cypress/no-unnecessary-waiting
+          // cy.wait(0);
+        }
+      });
+
+      it("Affected siblings are lifted up", () => {
+        cy.get("#item-rest-top").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 174.375)"
+        );
+
+        cy.get("#item-rest-all").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -166.375)"
+        );
+      });
+
+      it("Triggers mouseup", () => {
+        cy.get("#item-rest-top-bottom").trigger("mouseup", {
+          force: true,
+        });
+      });
+
+      it("Getting element again, transform it to the top", () => {
+        cy.get("#item-rest-top-bottom").trigger("mousedown", {
+          button: 0,
+        });
+
+        for (let i = 0; i <= 10; i += 1) {
+          cy.get("#item-rest-top-bottom").trigger("mousemove", {
+            clientY: startingPointY - i,
+            force: true,
+          });
+          // eslint-disable-next-line cypress/no-unnecessary-waiting
+          // cy.wait(0);
+        }
+      });
+
+      it("Dragged has not moved", () => {
+        cy.get("#item-rest-top-bottom").should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -83.1875)"
+        );
+      });
+
+      it("Triggers mouseup", () => {
+        cy.get("#item-rest-top-bottom").trigger("mouseup", {
+          force: true,
+        });
+      });
     });
   }
 );
