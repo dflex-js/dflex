a9fd5296e6beaf4d0085191f2806bbf4b78bf789
Jalal <jimmy002020@gmail.com>
2022-03-20 00:00:24 +0200

    Use `swc` for `Jest` and remove `Babel` (#424)

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  roots: ["<rootDir>packages"],
-  moduleNameMapper: {
-    "@dflex/(.+)$": "<rootDir>packages/$1/src",
-  },
-  moduleFileExtensions: ["ts", "js"],
-  testPathIgnorePatterns: ["cypress", "site", "test/integration"],
-};
