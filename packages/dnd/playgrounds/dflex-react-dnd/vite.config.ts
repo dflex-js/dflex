eb4fdbf6c4379f55d82e6bb421b5fbe7acbcc8cf
jalal246 <jimmy002020@gmail.com>
2022-04-04 09:29:07 +0200

    rm cra, use vite, fix export module in dnd package and root package.

---
+++ packages/dnd/playgrounds/dflex-react-dnd/vite.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3001,
+  },
+});
