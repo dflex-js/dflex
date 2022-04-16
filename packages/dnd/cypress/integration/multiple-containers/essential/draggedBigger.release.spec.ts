54dbc98bf535286c1e16fc96d906fb3c13492a07
jalal246 <jimmy002020@gmail.com>
2022-04-16 08:10:30 +0200

    it falis because of how the undo detect "not settled"

--- packages/dnd/cypress/integration/multiple-containers/essential/draggedBigger.release.spec.ts
+++ packages/dnd/cypress/integration/multiple-containers/essential/draggedBigger.release.spec.ts
@@ -34,7 +34,7 @@ context("Transforming into different container", () => {
       cy.wait(0);
     }
 
-    stepsX = 230;
+    stepsX = 231;
     for (let i = 0; i < stepsX; i += 10) {
       cy.get("#c1-1").trigger("mousemove", {
         clientX: startingPointX + i,
@@ -43,5 +43,61 @@ context("Transforming into different container", () => {
       // eslint-disable-next-line cypress/no-unnecessary-waiting
       cy.wait(0);
     }
+  });
+
+  it("Transforms element (#c1-1) - inside container-2", () => {
+    // stepsY = 110;
+    for (let i = stepsY; i >= 0; i -= 10) {
+      cy.get("#c1-1").trigger("mousemove", {
+        clientY: startingPointY - i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+  });
+
+  it("Triggers mouseup event", () => {
+    cy.get("#c1-1").trigger("mouseup", { force: true });
+  });
+
+  it("Dragged takes new position in the new container-2", () => {
+    cy.get("#c1-1").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 226, 0)"
+    );
+  });
+
+  it("Siblings shifted down to place the new element", () => {
+    cy.get("#c2-1").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 92)"
+    );
+
+    cy.get("#c2-2").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 92)"
+    );
+
+    cy.get("#c2-3").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 92)"
+    );
+
+    cy.get("#c2-4").should(
+      "have.css",
+      "transform",
+      "matrix(1, 0, 0, 1, 0, 92)"
+    );
+
+    // cy.get("#c2-5").should(
+    //   "have.css",
+    //   "transform",
+    //   "matrix(1, 0, 0, 1, 0, 92)"
+    // );
   });
 });
