51b48659620c31d0505f002df0932406d2f090aa
jalal246 <jimmy002020@gmail.com>
2022-04-17 08:42:12 +0200

    update deps

--- packages/dnd/playgrounds/dflex-react-dnd/src/main.tsx
+++ packages/dnd/playgrounds/dflex-react-dnd/src/main.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import "./index.css";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
