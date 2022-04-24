8e900d3d45547b7a2223ab304c32d6efe4561a31
jalal246 <jimmy002020@gmail.com>
2022-04-24 05:11:00 +0200

    add transfomation test

--- packages/dnd/cypress/integration/multiple-containers/soloCases/passingDirectContainer.spec.ts
+++ packages/dnd/cypress/integration/multiple-containers/soloCases/passingDirectContainer.spec.ts
@@ -1,1 +1,63 @@
-context("Passing containers while transforming", () => {});
+context("Passing containers while transforming", () => {
+  let elmBox: DOMRect;
+  let startingPointX: number;
+  let startingPointY: number;
+
+  let stepsX = 0;
+  let stepsY = 0;
+
+  before(() => {
+    cy.visit("http://localhost:3001/migration");
+  });
+
+  it("Getting the first element from container-1", () => {
+    cy.get("#c3-1").then((elm) => {
+      elmBox = elm[0].getBoundingClientRect();
+      // eslint-disable-next-line no-unused-vars
+      startingPointX = elmBox.x + elmBox.width / 2;
+      startingPointY = elmBox.y + elmBox.height / 2;
+
+      cy.get("#c3-1").trigger("mousedown", {
+        button: 0,
+      });
+    });
+  });
+
+  it("Transforms element (#c3-1) - outside the list above container-2", () => {
+    stepsY = 90;
+    for (let i = 0; i < stepsY; i += 10) {
+      cy.get("#c3-1").trigger("mousemove", {
+        clientY: startingPointY - i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+
+    stepsX = 470;
+    for (let i = 0; i < stepsX; i += 10) {
+      cy.get("#c3-1").trigger("mousemove", {
+        clientX: startingPointX - i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+  });
+
+  it("Transforms element (#c3-1) - inside container-2", () => {
+    // stepsY = 110;
+    for (let i = stepsY; i >= 0; i -= 10) {
+      cy.get("#c3-1").trigger("mousemove", {
+        clientY: startingPointY - i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+  });
+
+  it("Triggers mouseup event", () => {
+    cy.get("#c3-1").trigger("mouseup", { force: true });
+  });
+});
