ff41619d45b35b9415b12d4c64e0490c14ac3a2e
jalal246 <jimmy002020@gmail.com>
2022-04-24 07:09:32 +0200

    Split continer while transform

---
+++ packages/dnd/cypress/integration/multiple-containers/cases/split.spec.ts
@@ -0,0 +1,37 @@
+context("Split containers during transformation", () => {
+  let elmBox: DOMRect;
+  let startingPointX: number;
+  let startingPointY: number;
+
+  let stepsX = 0;
+  const stepsY = 0;
+
+  before(() => {
+    cy.visit("http://localhost:3001/migration");
+  });
+
+  it("Getting the first element from container-1", () => {
+    cy.get("#c3-1").then((elm) => {
+      elmBox = elm[0].getBoundingClientRect();
+      // eslint-disable-next-line no-unused-vars
+      startingPointX = elmBox.x + elmBox.width / 2;
+      startingPointY = elmBox.y + elmBox.height / 2;
+
+      cy.get("#c3-1").trigger("mousedown", {
+        button: 0,
+      });
+    });
+  });
+
+  it("Transforms element (#c3-1) - outside the horizontally", () => {
+    stepsX = 670;
+    for (let i = 0; i < stepsX; i += 10) {
+      cy.get("#c3-1").trigger("mousemove", {
+        clientX: startingPointX - i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+  });
+});
