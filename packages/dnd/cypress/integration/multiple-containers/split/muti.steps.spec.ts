6e609cf45ab78a0e2a6e032c6bdd43981a8a1b89
jalal246 <jimmy002020@gmail.com>
2022-04-25 05:34:54 +0200

    wip

--- packages/dnd/cypress/integration/multiple-containers/split/muti.steps.spec.ts
+++ packages/dnd/cypress/integration/multiple-containers/split/muti.steps.spec.ts
@@ -1,10 +1,10 @@
-context("Split containers during transformation", () => {
+context.skip("Split containers during transformation", () => {
   let elmBox: DOMRect;
   let startingPointX: number;
-  let startingPointY: number;
+  // let startingPointY: number;
 
   let stepsX = 0;
-  let stepsY = 0;
+  // const stepsY = 0;
 
   before(() => {
     cy.visit("http://localhost:3001/migration");
@@ -15,7 +15,7 @@ context("Split containers during transformation", () => {
       elmBox = elm[0].getBoundingClientRect();
       // eslint-disable-next-line no-unused-vars
       startingPointX = elmBox.x + elmBox.width / 2;
-      startingPointY = elmBox.y + elmBox.height / 2;
+      // startingPointY = elmBox.y + elmBox.height / 2;
 
       cy.get("#c3-1").trigger("mousedown", {
         button: 0,
@@ -34,14 +34,14 @@ context("Split containers during transformation", () => {
       cy.wait(0);
     }
 
-    stepsY = 10;
-    for (let i = stepsY; i >= 0; i -= 10) {
-      cy.get("#c3-1").trigger("mousemove", {
-        clientY: startingPointY + i,
-        force: true,
-      });
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(0);
-    }
+    // stepsY = 10;
+    // for (let i = stepsY; i >= 0; i -= 10) {
+    //   cy.get("#c3-1").trigger("mousemove", {
+    //     clientY: startingPointY + i,
+    //     force: true,
+    //   });
+    //   // eslint-disable-next-line cypress/no-unnecessary-waiting
+    //   cy.wait(0);
+    // }
   });
 });
