1a9bdda1ad3e2ee9c33b6a08c59de94d15b00cfd
jalal246 <jimmy002020@gmail.com>
2022-05-04 12:03:30 +0200

    wip test

--- packages/dnd/cypress/integration/multiple-containers/backAndForth/toSmallerList/horizontally.bottom.spec.ts
+++ packages/dnd/cypress/integration/multiple-containers/backAndForth/toSmallerList/horizontally.bottom.spec.ts
@@ -6,7 +6,7 @@ context(
     let startingPointY: number;
 
     let stepsX = 0;
-    // const stepsY = 0;
+    let stepsY = 0;
 
     before(() => {
       cy.visit("http://localhost:3001/migration");
@@ -118,6 +118,49 @@ context(
 
         expect(newElmBox).to.deep.equal(elmBox);
       });
+    });
+
+    it("Transforms element (#c2-5) - up in its origin", () => {
+      cy.get("#c2-5").trigger("mousedown", {
+        button: 0,
+      });
+
+      stepsY = 420;
+      for (let i = 0; i < stepsY; i += 10) {
+        cy.get("#c1-1").trigger("mousemove", {
+          clientX: startingPointX,
+          clientY: startingPointY - i,
+          force: true,
+        });
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(0);
+      }
+    });
+
+    it("Siblings should be lifted up", () => {
+      cy.get("#c2-1").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+
+      cy.get("#c2-2").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+
+      cy.get("#c2-3").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
+
+      cy.get("#c2-4").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 0)"
+      );
     });
   }
 );
