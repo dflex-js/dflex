d440ee1704e7c3fa2e717e6fbe18e8fc0ded0eba
Jalal <jimmy002020@gmail.com>
2022-05-10 09:10:12 +0200

    Initiate obliquity test cases (#522)

---
+++ packages/dnd/cypress/integration/multiple-containers/obliquity/bottomUp/multi.spec.ts
@@ -0,0 +1,52 @@
+context("Split multiple containers form bottom up", () => {
+  before(() => {
+    cy.visit("http://localhost:3001/migration");
+  });
+
+  let elmBox: DOMRect;
+  let startingPointX: number;
+  let startingPointY: number;
+
+  let stepsX = 0;
+  let stepsY = 0;
+
+  it("Getting the first element from container-3", () => {
+    cy.get("#c3-2").then((elm) => {
+      elmBox = elm[0].getBoundingClientRect();
+      // eslint-disable-next-line no-unused-vars
+      startingPointX = elmBox.x + elmBox.width / 2;
+      startingPointY = elmBox.y + elmBox.height / 2;
+
+      cy.get("#c3-2").trigger("mousedown", {
+        button: 0,
+      });
+    });
+  });
+
+  it("Transforms element (#c3-2) into containers-1", () => {
+    stepsY = 280;
+    for (let i = 0; i < stepsY; i += 10) {
+      cy.get("#c3-2").trigger("mousemove", {
+        clientY: startingPointY + i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+
+    stepsX = 420;
+    for (let i = 0; i < stepsX; i += 10) {
+      cy.get("#c3-2").trigger("mousemove", {
+        clientY: startingPointY + stepsY - i,
+        clientX: startingPointX - i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+  });
+
+  it("Triggers mouseup event", () => {
+    cy.get("#c3-2").trigger("mouseup", { force: true });
+  });
+});
