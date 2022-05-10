ae995ce4eba1b5f942799fa0128a7840e057d1aa
jalal246 <jimmy002020@gmail.com>
2022-05-10 08:36:21 +0200

    add new cases for multi and mono

---
+++ packages/dnd/cypress/integration/multiple-containers/obliquity/oneStep.bottomUp.spec.ts
@@ -0,0 +1,42 @@
+context("Split one container form bottom up", () => {
+  before(() => {
+    cy.visit("http://localhost:3001/migration");
+  });
+
+  let elmBox: DOMRect;
+  let startingPointX: number;
+  let startingPointY: number;
+
+  // const stepsX = 0;
+  let stepsY = 0;
+
+  it("Getting the first element from container-3", () => {
+    cy.get("#c2-3").then((elm) => {
+      elmBox = elm[0].getBoundingClientRect();
+      // eslint-disable-next-line no-unused-vars
+      startingPointX = elmBox.x + elmBox.width / 2;
+      startingPointY = elmBox.y + elmBox.height / 2;
+
+      cy.get("#c2-3").trigger("mousedown", {
+        button: 0,
+      });
+    });
+  });
+
+  it("Transforms element (#c2-3) into containers-1", () => {
+    stepsY = 130;
+    for (let i = 0; i < stepsY; i += 1) {
+      cy.get("#c2-3").trigger("mousemove", {
+        clientY: startingPointY - i,
+        clientX: startingPointX - i,
+        force: true,
+      });
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(0);
+    }
+  });
+
+  it("Triggers mouseup event", () => {
+    cy.get("#c2-3").trigger("mouseup", { force: true });
+  });
+});
