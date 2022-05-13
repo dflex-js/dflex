cee660517d3b36bd38c9f367187f0f5e52179aad
jalal246 <jimmy002020@gmail.com>
2022-05-13 09:19:44 +0200

    refactor unit test

--- packages/dnd/test/Store/dndStore.test.ts
+++ packages/dnd/test/Store/dndStore.test.ts
@@ -1,22 +1,35 @@
 import store from "../../src/DnDStore";
 
-import { elm1, elm2, elm3, elm4 } from "../utils";
+import { elm1 } from "../__mocks__/DOMElements";
 
 describe("DnD Store", () => {
   beforeAll(() => {
     store.register(elm1);
-    store.register(elm2);
-    store.register(elm3);
-    store.register(elm4);
   });
 
   it("Element is initiated", () => {
+    store.register(elm1);
     expect(store.registry).toMatchSnapshot();
   });
 
-  it("Returns all element connections and instance", () => {
-    const elemInstance = store.getElmTreeById(elm1.id);
+  it("Returns element branch and parent", () => {
+    const { branches, parent } = store.getElmTreeById(elm1.id);
 
-    expect(elemInstance).toMatchSnapshot();
+    expect(branches).toMatchInlineSnapshot(`
+      Object {
+        "parents": undefined,
+        "siblings": Array [
+          "id-1",
+        ],
+      }
+    `);
+
+    expect(parent).toMatchInlineSnapshot(`null`);
   });
+
+  // it("Registers more elements in the store all in the same level (depth=0)", () => {
+  //   store.register(elm2);
+  //   store.register(elm3);
+  //   store.register(elm4);
+  // });
 });
