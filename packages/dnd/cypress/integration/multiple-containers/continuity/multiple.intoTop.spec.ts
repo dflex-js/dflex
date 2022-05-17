450a9a7bc1ddaa6de5acb68e243ffbf9f98ae666
Jalal <jimmy002020@gmail.com>
2022-05-17 13:56:42 +0200

    Fix continuity in extending orphan container (#534)

--- packages/dnd/cypress/integration/multiple-containers/continuity/multiple.intoTop.spec.ts
+++ packages/dnd/cypress/integration/multiple-containers/continuity/multiple.intoTop.spec.ts
@@ -12,108 +12,140 @@ context(
       cy.visit("http://localhost:3001/migration");
     });
 
-    context("Transform from to container-3 into (2)", () => {
-      it("Getting the first element from container-1", () => {
-        cy.get("#c3-1").then((elm) => {
-          elmBox = elm[0].getBoundingClientRect();
-          // eslint-disable-next-line no-unused-vars
-          startingPointX = elmBox.x + elmBox.width / 2;
-          startingPointY = elmBox.y + elmBox.height / 2;
+    it("Getting the first element from container-1", () => {
+      cy.get("#c3-1").then((elm) => {
+        elmBox = elm[0].getBoundingClientRect();
+        // eslint-disable-next-line no-unused-vars
+        startingPointX = elmBox.x + elmBox.width / 2;
+        startingPointY = elmBox.y + elmBox.height / 2;
 
-          cy.get("#c3-1").trigger("mousedown", {
-            button: 0,
-          });
+        cy.get("#c3-1").trigger("mousedown", {
+          button: 0,
         });
       });
+    });
 
-      it("Transforms element (#c3-1) - outside the list above container-3", () => {
-        stepsY = 90;
-        for (let i = 0; i < stepsY; i += 10) {
-          cy.get("#c3-1").trigger("mousemove", {
-            clientY: startingPointY - i,
-            force: true,
-          });
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
-        }
+    it("Transforms element (#c3-1) - outside the list above container-3", () => {
+      stepsY = 90;
+      for (let i = 0; i < stepsY; i += 10) {
+        cy.get("#c3-1").trigger("mousemove", {
+          clientY: startingPointY - i,
+          force: true,
+        });
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(0);
+      }
 
-        stepsX = 470;
-        for (let i = 0; i < stepsX; i += 10) {
-          cy.get("#c3-1").trigger("mousemove", {
-            clientX: startingPointX - i,
-            force: true,
-          });
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
-        }
-      });
+      stepsX = 470;
+      for (let i = 0; i < stepsX; i += 10) {
+        cy.get("#c3-1").trigger("mousemove", {
+          clientX: startingPointX - i,
+          force: true,
+        });
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(0);
+      }
+    });
 
-      it("Transforms element (#c3-1) - inside container-1", () => {
-        // stepsY = 110;
-        for (let i = stepsY; i >= 0; i -= 10) {
-          cy.get("#c3-1").trigger("mousemove", {
-            clientY: startingPointY - i,
-            force: true,
-          });
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
-        }
-      });
+    it("Transforms element (#c3-1) - inside container-1", () => {
+      // stepsY = 110;
+      for (let i = stepsY; i >= 0; i -= 10) {
+        cy.get("#c3-1").trigger("mousemove", {
+          clientY: startingPointY - i,
+          force: true,
+        });
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(0);
+      }
+    });
 
-      it("Triggers mouseup event", () => {
-        cy.get("#c3-1").trigger("mouseup", { force: true });
-      });
+    it("Triggers mouseup event", () => {
+      cy.get("#c3-1").trigger("mouseup", { force: true });
+    });
 
-      it("Getting the first element from container-1", () => {
-        cy.get("#c3-2").then((elm) => {
-          elmBox = elm[0].getBoundingClientRect();
-          // eslint-disable-next-line no-unused-vars
-          startingPointX = elmBox.x + elmBox.width / 2;
-          startingPointY = elmBox.y + elmBox.height / 2;
+    it("Getting the first element from container-1", () => {
+      cy.get("#c3-2").then((elm) => {
+        elmBox = elm[0].getBoundingClientRect();
+        // eslint-disable-next-line no-unused-vars
+        startingPointX = elmBox.x + elmBox.width / 2;
+        startingPointY = elmBox.y + elmBox.height / 2;
 
-          cy.get("#c3-2").trigger("mousedown", {
-            button: 0,
-          });
+        cy.get("#c3-2").trigger("mousedown", {
+          button: 0,
         });
       });
+    });
 
-      it("Transforms element (#c3-2) - outside the list above container-3", () => {
-        stepsY = 90;
-        for (let i = 0; i < stepsY; i += 10) {
-          cy.get("#c3-2").trigger("mousemove", {
-            clientY: startingPointY - i,
-            force: true,
-          });
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
-        }
+    it("Transforms element (#c3-2) - outside the list above container-3", () => {
+      stepsY = 90;
+      for (let i = 0; i < stepsY; i += 10) {
+        cy.get("#c3-2").trigger("mousemove", {
+          clientY: startingPointY - i,
+          force: true,
+        });
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(0);
+      }
 
-        stepsX = 470;
-        for (let i = 0; i < stepsX; i += 10) {
-          cy.get("#c3-2").trigger("mousemove", {
-            clientX: startingPointX - i,
-            force: true,
-          });
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
-        }
-      });
+      stepsX = 470;
+      for (let i = 0; i < stepsX; i += 10) {
+        cy.get("#c3-2").trigger("mousemove", {
+          clientX: startingPointX - i,
+          force: true,
+        });
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(0);
+      }
+    });
 
-      it("Transforms element (#c3-2) - inside container-1", () => {
-        // stepsY = 110;
-        for (let i = stepsY; i >= 0; i -= 10) {
-          cy.get("#c3-2").trigger("mousemove", {
-            clientY: startingPointY - i,
-            force: true,
-          });
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(0);
-        }
-      });
+    it("Transforms element (#c3-2) - inside container-1", () => {
+      // stepsY = 110;
+      for (let i = stepsY; i >= 0; i -= 10) {
+        cy.get("#c3-2").trigger("mousemove", {
+          clientY: startingPointY - i,
+          force: true,
+        });
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(0);
+      }
+    });
 
-      it("Triggers mouseup event", () => {
-        cy.get("#c3-2").trigger("mouseup", { force: true });
-      });
+    it("Triggers mouseup event", () => {
+      cy.get("#c3-2").trigger("mouseup", { force: true });
+    });
+
+    it("Siblings in container 2 stay the same", () => {
+      cy.get("#c2-1").should("have.css", "transform", "none");
+
+      cy.get("#c2-2").should("have.css", "transform", "none");
+
+      cy.get("#c2-3").should("have.css", "transform", "none");
+
+      cy.get("#c2-4").should("have.css", "transform", "none");
+
+      cy.get("#c2-5").should("have.css", "transform", "none");
+    });
+
+    it("Siblings in destination have the correct position", () => {
+      cy.get("#c1-1").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, 0, 224)"
+      );
+    });
+
+    it("Dragged elements have the correct position", () => {
+      cy.get("#c3-1").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, -452, 112)"
+      );
+
+      cy.get("#c3-2").should(
+        "have.css",
+        "transform",
+        "matrix(1, 0, 0, 1, -452, -112)"
+      );
     });
   }
 );
