3686b6a64e83780de0f0fe254cc3f267d37c10ae
jalal246 <jimmy002020@gmail.com>
2022-06-04 11:46:35 +0200

    wip

---
+++ packages/dflex-dnd-playground/cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
