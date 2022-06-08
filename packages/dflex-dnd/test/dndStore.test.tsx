c1b1e00a0a0d73aa2a676aef6570778fb5467101
jalal246 <jimmy002020@gmail.com>
2022-06-08 18:02:31 +0200

    wip

--- packages/dflex-dnd/test/dndStore.test.tsx
+++ packages/dflex-dnd/test/dndStore.test.tsx
@@ -1,13 +1,83 @@
-import store from "../src/DnDStore";
-// import DraggableInteractive from "../src/Draggable";
-// import DistanceCalculator from "../src/Droppable/DistanceCalculator";
-// import extractOpts from "../src/utils/extractOpts";
+/* eslint-disable import/no-extraneous-dependencies */
+import * as React from "react";
+import * as ReactTestUtils from "react-dom/test-utils";
+import { Root, createRoot } from "react-dom/client";
 
-import { elm1, elm2, elm3, elm4 } from "./__mocks__/DOMElements";
+import store from "../src/DnDStore";
 
 describe("DnD Store", () => {
+  const elm1 = {
+    id: "id-1",
+    depth: 0,
+    parentID: "",
+    readonly: false,
+  };
+
+  const elm2 = {
+    id: "id-2",
+    depth: 0,
+    parentID: "",
+    readonly: false,
+  };
+
+  const elm3 = {
+    id: "id-3",
+    depth: 0,
+    parentID: "",
+    readonly: false,
+  };
+
+  const elm4 = {
+    id: "id-4",
+    depth: 0,
+    parentID: "",
+    readonly: false,
+  };
+
+  let container: HTMLDivElement | null;
+  let reactRoot: Root;
+
+  const positions: { [k: string]: DOMRect } = {};
+
   beforeAll(() => {
-    store.register(elm1);
+    container = document.createElement("div");
+    reactRoot = createRoot(container);
+    document.body.appendChild(container);
+
+    function render(elm: typeof elm3) {
+      const ref = React.createRef<HTMLDivElement>();
+
+      React.useEffect(() => {
+        if (ref.current) {
+          store.register(elm);
+
+          positions[elm.id] = ref.current.getBoundingClientRect();
+        }
+
+        return () => {
+          store.unregister(elm.id);
+        };
+      }, [ref]);
+
+      return <div id={elm.id} key={elm.id} ref={ref} />;
+    }
+
+    function init() {
+      function TestBase() {
+        return <div>{[elm1, elm2, elm3, elm4].map((elm) => render(elm))}</div>;
+      }
+
+      ReactTestUtils.act(() => {
+        reactRoot.render(<TestBase />);
+      });
+    }
+
+    init();
+  });
+
+  afterAll(() => {
+    document.body.removeChild(container!);
+    container = null;
   });
 
   describe("Shallow test the store", () => {
@@ -20,13 +90,16 @@ describe("DnD Store", () => {
       const { branches, parent } = store.getElmTreeById(elm1.id);
 
       expect(branches).toMatchInlineSnapshot(`
-      Object {
-        "parents": undefined,
-        "siblings": Array [
-          "id-1",
-        ],
-      }
-    `);
+        Object {
+          "parents": undefined,
+          "siblings": Array [
+            "id-1",
+            "id-2",
+            "id-3",
+            "id-4",
+          ],
+        }
+      `);
 
       expect(parent).toMatchInlineSnapshot(`null`);
     });
@@ -53,17 +126,17 @@ describe("DnD Store", () => {
         const { elm, ...rest } = store.getInsertionELmMeta(0, SK);
 
         expect(rest).toMatchInlineSnapshot(`
-                  Object {
-                    "isEmpty": false,
-                    "isOrphan": true,
-                    "isRestoredLastPosition": false,
-                    "position": PointNum {
-                      "x": 450,
-                      "y": 114,
-                    },
-                    "prevElm": null,
-                  }
-              `);
+          Object {
+            "isEmpty": false,
+            "isOrphan": false,
+            "isRestoredLastPosition": false,
+            "position": PointNum {
+              "x": 0,
+              "y": 0,
+            },
+            "prevElm": null,
+          }
+        `);
       });
 
       it("Register three elements in the store", () => {
@@ -95,15 +168,15 @@ describe("DnD Store", () => {
                   }
               `);
 
-        const pos = elm4.ref.getBoundingClientRect();
+        const pos = positions[elm4.id];
 
         expect(position.getInstance()).toStrictEqual({
           x: pos.left,
           y: pos.top,
         });
 
-        expect(elm.id).toBe(elm4.id);
-        expect(prevElm.id).toBe(elm3.id);
+        expect(elm!.id).toBe(elm4.id);
+        expect(prevElm!.id).toBe(elm3.id);
       });
 
       it("Preserve the last element", () => {
@@ -127,8 +200,8 @@ describe("DnD Store", () => {
 
         expect(position.getInstance()).toStrictEqual(preservePosition);
 
-        expect(elm.id).toBe(elm4.id);
-        expect(prevElm.id).toBe(elm3.id);
+        expect(elm!.id).toBe(elm4.id);
+        expect(prevElm!.id).toBe(elm3.id);
       });
 
       it("Restores the position even preserved is defined because it's not the last element", () => {
@@ -145,15 +218,15 @@ describe("DnD Store", () => {
                   }
               `);
 
-        const pos = elm3.ref.getBoundingClientRect();
+        const pos = positions[elm3.id];
 
         expect(position.getInstance()).toStrictEqual({
           x: pos.left,
           y: pos.top,
         });
 
-        expect(elm.id).toBe(elm3.id);
-        expect(prevElm.id).toBe(elm2.id);
+        expect(elm!.id).toBe(elm3.id);
+        expect(prevElm!.id).toBe(elm2.id);
       });
 
       it("Restores the position for the first element with correct flags", () => {
@@ -170,14 +243,14 @@ describe("DnD Store", () => {
                   }
               `);
 
-        const pos = elm1.ref.getBoundingClientRect();
+        const pos = positions[elm1.id];
 
         expect(position.getInstance()).toStrictEqual({
           x: pos.left,
           y: pos.top,
         });
 
-        expect(elm.id).toBe(elm1.id);
+        expect(elm!.id).toBe(elm1.id);
         expect(prevElm).toBeNull();
       });
     });
@@ -211,13 +284,15 @@ describe("DnD Store", () => {
                   }
               `);
 
+        const pos = positions[elm4.id];
+
         expect(position.getInstance()).toStrictEqual({
-          x: elm4.ref.getBoundingClientRect().left,
-          y: elm4.ref.getBoundingClientRect().top,
+          x: pos.left,
+          y: pos.top,
         });
 
-        expect(elm.id).toBe(elm4.id);
-        expect(prevElm.id).toBe(elm3.id);
+        expect(elm!.id).toBe(elm4.id);
+        expect(prevElm!.id).toBe(elm3.id);
       });
 
       it("Restores the position even preserved is defined because it's not the last element", () => {
@@ -234,15 +309,15 @@ describe("DnD Store", () => {
                   }
               `);
 
-        const pos = elm4.ref.getBoundingClientRect();
+        const pos = positions[elm4.id];
 
         expect(position.getInstance()).toStrictEqual({
           x: pos.left,
           y: pos.top,
         });
 
-        expect(elm.id).toBe(elm4.id);
-        expect(prevElm.id).toBe(elm3.id);
+        expect(elm!.id).toBe(elm4.id);
+        expect(prevElm!.id).toBe(elm3.id);
       });
 
       it("Restores the position for the first element with correct flags", () => {
@@ -259,14 +334,14 @@ describe("DnD Store", () => {
                   }
               `);
 
-        const pos = elm1.ref.getBoundingClientRect();
+        const pos = positions[elm1.id];
 
         expect(position.getInstance()).toStrictEqual({
           x: pos.left,
           y: pos.top,
         });
 
-        expect(elm.id).toBe(elm1.id);
+        expect(elm!.id).toBe(elm1.id);
         expect(prevElm).toBeNull();
       });
     });
