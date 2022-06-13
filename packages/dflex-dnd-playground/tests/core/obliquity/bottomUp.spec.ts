665080005c67cc8fb910150e51e446e7ecbd537d
jalal246 <jimmy002020@gmail.com>
2022-06-13 14:41:04 +0200

    wip

---
+++ packages/dflex-dnd-playground/tests/core/obliquity/bottomUp.spec.ts
@@ -0,0 +1,77 @@
+import { test, expect, Page, Locator } from "@playwright/test";
+import { getDraggedRect, initialize, moveDragged } from "../utils";
+
+test.describe.serial.only("Dragging from bottom up", async () => {
+  let page: Page;
+
+  const draggedID = "#id-12";
+
+  let elm10: Locator;
+  let elm09: Locator;
+  let elm11: Locator;
+  let elm12: Locator;
+
+  test.beforeAll(async ({ browser, baseURL }) => {
+    const context = await browser.newContext();
+
+    page = await context.newPage();
+    initialize(page, 10);
+    await page.goto(baseURL!);
+
+    [elm09, elm10, elm11, elm12] = await Promise.all([
+      page.locator("#id-9"),
+      page.locator("#id-10"),
+      page.locator("#id-11"),
+      page.locator("#id-12"),
+    ]);
+  });
+
+  test("Moving dragged element outside its position", async () => {
+    await getDraggedRect(elm12);
+    await moveDragged(-35, -35);
+  });
+
+  test("Checking the stability of the new positions", async () => {
+    await expect(elm09).toHaveCSS("transform", "none");
+    await expect(elm10).toHaveCSS("transform", "none");
+    await expect(elm11).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 58)");
+  });
+
+  test("Continue transformation to the top below #9", async () => {
+    await moveDragged(-90, -90);
+  });
+
+  test("Transform elm#10 and elm#11 once it's entering the threshold", async () => {
+    await expect(elm09).toHaveCSS("transform", "none");
+    await expect(elm10).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 58)");
+    await expect(elm11).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 58)");
+  });
+
+  test("Continue transformation to the top outside the container", async () => {
+    await moveDragged(-190, -190);
+  });
+
+  test("Transform elm#10 and elm#11 back to their positions", async () => {
+    await expect(elm09).toHaveCSS("transform", "none");
+    await expect(elm10).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 0)");
+    await expect(elm11).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 0)");
+  });
+
+  test("Insert dragged into elm#9 breaking point", async () => {
+    await moveDragged(0, -190);
+  });
+
+  test("Triggers mouseup", async () => {
+    await page.dispatchEvent(draggedID, "mouseup", {
+      button: 0,
+      force: true,
+    });
+  });
+
+  test("Siblings positioned correctly", async () => {
+    await expect(elm09).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 58)");
+    await expect(elm10).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 58)");
+    await expect(elm11).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, 58)");
+    await expect(elm12).toHaveCSS("transform", "matrix(1, 0, 0, 1, 0, -174)");
+  });
+});
