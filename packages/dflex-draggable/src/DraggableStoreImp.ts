0a9e13f8d517d3acefffbf2a430e9b9d63b44606
jalal246 <jimmy002020@gmail.com>
2022-06-16 21:20:26 +0200

    Fix Draggable type

--- packages/dflex-draggable/src/DraggableStoreImp.ts
+++ packages/dflex-draggable/src/DraggableStoreImp.ts
@@ -10,10 +10,6 @@ class DraggableStoreImp extends DFlexBaseStore {
       id,
       depth: 0,
       parentID: "",
-      isInitialized: true,
-      isPaused: false,
-      scrollX: 0,
-      scrollY: 0,
       readonly: false,
     });
   }
