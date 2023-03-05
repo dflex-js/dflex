8b7b66bdc33e2d68ed008b112a9dec75ca96acf6
jalal246 <jimmy002020@gmail.com>
2023-03-05 20:11:11 +0200

    wip

--- packages/dflex-utils/src/Box/BoxNum.ts
+++ packages/dflex-utils/src/Box/BoxNum.ts
@@ -2,30 +2,19 @@ import type AbstractBox from "./AbstractBox";
 import Box from "./Box";
 
 class BoxNum extends Box<number> {
-  clone(box: AbstractBox) {
-    this.left = box.left;
-    this.top = box.top;
-    this.right = box.right;
-    this.bottom = box.bottom;
-  }
-
-  hasSamePoint(box: AbstractBox) {
-    return this.left === box.left && this.top === box.top;
-  }
-
-  isUnder(box: AbstractBox) {
+  private _isUnder(box: AbstractBox) {
     return this.top > box.bottom;
   }
 
-  isAbove(box: AbstractBox) {
+  private _isAbove(box: AbstractBox) {
     return this.bottom < box.top;
   }
 
-  isOnLeft(box: AbstractBox) {
+  private _isOnLeft(box: AbstractBox) {
     return this.right < box.left;
   }
 
-  isOneRight(box: AbstractBox) {
+  private _isOneRight(box: AbstractBox) {
     return this.left > box.right;
   }
 
@@ -37,10 +26,10 @@ class BoxNum extends Box<number> {
    */
   isNotIntersect(box: AbstractBox): boolean {
     return (
-      this.isUnder(box) ||
-      this.isOnLeft(box) ||
-      this.isAbove(box) ||
-      this.isOneRight(box)
+      this._isUnder(box) ||
+      this._isOnLeft(box) ||
+      this._isAbove(box) ||
+      this._isOneRight(box)
     );
   }
 
@@ -60,14 +49,14 @@ class BoxNum extends Box<number> {
    * @param box
    * @returns
    */
-  isInside(box: AbstractBox): boolean {
-    return (
-      this.top >= box.top &&
-      this.right <= box.right &&
-      this.bottom <= box.bottom &&
-      this.left >= box.left
-    );
-  }
+  // isInside(box: AbstractBox): boolean {
+  //   return (
+  //     this.top >= box.top &&
+  //     this.right <= box.right &&
+  //     this.bottom <= box.bottom &&
+  //     this.left >= box.left
+  //   );
+  // }
 
   /**
    * True when it's outside of other box.
@@ -75,12 +64,12 @@ class BoxNum extends Box<number> {
    * @param box
    * @returns
    */
-  isOutside(box: AbstractBox): boolean {
-    return !this.isInside(box);
-  }
+  // isOutside(box: AbstractBox): boolean {
+  //   return !this.isInside(box);
+  // }
 
   isPositionedY(box: AbstractBox) {
-    return this.isUnder(box) || this.isAbove(box);
+    return this._isUnder(box) || this._isAbove(box);
   }
 
   assignBiggestBox(box: AbstractBox) {
