17a6ef5e5e3bf3a99d5ea11e7168e6734bd95794
jalal246 <jimmy002020@gmail.com>
2023-05-24 01:04:56 +0300

    does it work?

---
+++ packages/dflex-dnd-playground/tests/features/scrollV.spec.ts
@@ -0,0 +1,88 @@
+import {
+  test,
+  Page,
+  Locator,
+  BrowserContext,
+  Browser,
+  expect,
+} from "@playwright/test";
+
+import {
+  //   assertChildrenOrderIDs,
+  //   assertDefaultChildrenIndex,
+  getDraggedRect,
+  initialize,
+  moveDragged,
+} from "../utils";
+
+test.describe.serial("Drag the first element down vertically", async () => {
+  let page: Page;
+  let context: BrowserContext;
+  let activeBrowser: Browser;
+
+  let elementSelectors: string[];
+  let elements: Locator[];
+  let visibleElements: Locator[];
+  let invisibleElements: Locator[];
+
+  //   let elmParent: Locator;
+
+  test.beforeAll(async ({ browser, browserName }) => {
+    activeBrowser = browser;
+
+    context = await activeBrowser.newContext();
+    page = await context.newPage();
+    initialize(page, browserName, 50);
+    await page.goto("/extended");
+
+    elementSelectors = Array.from(
+      { length: 100 },
+      (_, index) => `[id='${index + 1}-extended']`
+    );
+
+    elements = await Promise.all(
+      elementSelectors.map((selector) => page.locator(selector))
+    );
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+    await context.close();
+    // await activeBrowser.close();
+  });
+
+  test("Move elm1 outside the container down into elm38", async () => {
+    await getDraggedRect(elements[0]);
+    const viewportHeight = page.viewportSize()!.height;
+    await moveDragged(-220, -1);
+    await moveDragged(-1, viewportHeight);
+    await page.waitForTimeout(1000);
+    await moveDragged(-1, viewportHeight / 2);
+    await page.waitForTimeout(1000);
+  });
+
+  test("Only visible siblings are transformed", async ({ browserName }) => {
+    const expectedResult =
+      browserName === "firefox"
+        ? "matrix(1, 0, 0, 1, 0, -59.2)"
+        : "matrix(1, 0, 0, 1, 0, -59.1875)";
+
+    visibleElements = elements.slice(2, 37);
+
+    await Promise.all(
+      visibleElements.map((element) =>
+        expect(element).toHaveCSS("transform", expectedResult)
+      )
+    );
+  });
+
+  test("Invisible siblings are not transformed", async () => {
+    invisibleElements = elements.slice(50);
+
+    await Promise.all(
+      invisibleElements.map((element) =>
+        expect(element).toHaveCSS("transform", "none")
+      )
+    );
+  });
+});
