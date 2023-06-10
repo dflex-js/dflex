7a5663c8e2b026a547a18df9224b2a3696d830bd
jalal246 <jimmy002020@gmail.com>
2023-06-10 19:50:59 +0300

    nicer shadow because why not

--- packages/dflex-dnd-playground/src/components/DFlexDnDComponent.tsx
+++ packages/dflex-dnd-playground/src/components/DFlexDnDComponent.tsx
@@ -49,7 +49,7 @@ const DFlexDnDComponent = ({
         readonly,
         dragCSS: {
           background: "#ae51ff",
-          "box-shadow": "0 0 8px 4px rgba(0, 0, 0, 0.3)",
+          "box-shadow": "0 0 8px 4px rgba(255, 255, 255, 0.5)",
         },
       });
     }
