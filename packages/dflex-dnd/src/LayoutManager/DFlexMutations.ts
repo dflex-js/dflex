88256290dd50ae005aa0249da54ec21ef1fae7bd
jalal246 <jimmy002020@gmail.com>
2023-06-30 23:10:02 +0300

    init

--- packages/dflex-dnd/src/LayoutManager/DFlexMutations.ts
+++ packages/dflex-dnd/src/LayoutManager/DFlexMutations.ts
@@ -12,7 +12,66 @@ function getIsProcessingMutations(): boolean {
   return isProcessingMutations;
 }
 
-function cleanupSiblings(store: DFlexDnDStore) {
+function cleanupLeaves(store: DFlexDnDStore, SK: string): [string[], string[]] {
+  const connectedNodesID: string[] = [];
+  const deletedNodesID: string[] = [];
+
+  const siblings = store.getElmSiblingsByKey(SK);
+
+  for (let i = 0; i < siblings.length; i += 1) {
+    const elmID = siblings[i];
+
+    if (terminatedDOMiDs.has(elmID)) {
+      deletedNodesID.push(elmID);
+    } else {
+      const dflexElm = store.registry.get(elmID)!;
+
+      const index = connectedNodesID.push(elmID) - 1;
+
+      if (index !== dflexElm.VDOMOrder.self) {
+        dflexElm.updateIndex(store.interactiveDOM.get(elmID)!, index);
+
+        if (featureFlags.enableRegisterDebugger) {
+          // eslint-disable-next-line no-console
+          console.log(`cleanupLeaves: updating index for ${elmID} to ${index}`);
+        }
+      }
+    }
+  }
+
+  return [connectedNodesID, deletedNodesID];
+}
+
+function cleanupSiblings(
+  store: DFlexDnDStore,
+  SK: string,
+  BK: string,
+  depth: number
+): void {
+  const [connectedNodesID, deletedNodesID] = cleanupLeaves(store, SK);
+
+  if (__DEV__) {
+    if (featureFlags.enableRegisterDebugger) {
+      // eslint-disable-next-line no-console
+      console.log(
+        `cleanupSiblings: Found ${connectedNodesID.length} connected`,
+        connectedNodesID
+      );
+    }
+  }
+
+  if (connectedNodesID.length > 0) {
+    store.mutateSiblings(SK, connectedNodesID);
+
+    deletedNodesID.forEach((id) => {
+      store.cleanupELmInstance(id, BK);
+    });
+  } else {
+    store.cleanupSiblingsInstance(SK, BK, depth);
+  }
+}
+
+function cleanupIDs(store: DFlexDnDStore): void {
   const SKeys = new Map<string, { BK: string; depth: number }>();
 
   terminatedDOMiDs.forEach((id) => {
@@ -26,7 +85,7 @@ function cleanupSiblings(store: DFlexDnDStore) {
     if (__DEV__) {
       if (featureFlags.enableRegisterDebugger) {
         // eslint-disable-next-line no-console
-        console.log(`cleanupSiblings: removing ${id} from registry`);
+        console.log(`cleanupIDs: removing ${id} from registry`);
       }
     }
 
@@ -35,55 +94,7 @@ function cleanupSiblings(store: DFlexDnDStore) {
     }
   });
 
-  SKeys.forEach(({ BK, depth }, SK) => {
-    const connectedNodesID: string[] = [];
-    const deletedNodesID: string[] = [];
-
-    const siblings = store.getElmSiblingsByKey(SK);
-
-    for (let i = 0; i < siblings.length; i += 1) {
-      const elmID = siblings[i];
-
-      if (terminatedDOMiDs.has(elmID)) {
-        deletedNodesID.push(elmID);
-      } else {
-        const dflexElm = store.registry.get(elmID)!;
-
-        const index = connectedNodesID.push(elmID) - 1;
-
-        if (index !== dflexElm.VDOMOrder.self) {
-          dflexElm.updateIndex(store.interactiveDOM.get(elmID)!, index);
-
-          if (featureFlags.enableRegisterDebugger) {
-            // eslint-disable-next-line no-console
-            console.log(
-              `cleanupSiblings: updating index for ${elmID} to ${index}`
-            );
-          }
-        }
-      }
-    }
-
-    if (__DEV__) {
-      if (featureFlags.enableRegisterDebugger) {
-        // eslint-disable-next-line no-console
-        console.log(
-          `cleanupSiblings: Found ${connectedNodesID.length} connected`,
-          connectedNodesID
-        );
-      }
-    }
-
-    if (connectedNodesID.length > 0) {
-      store.mutateSiblings(SK, connectedNodesID);
-
-      deletedNodesID.forEach((id) => {
-        store.cleanupELmInstance(id, BK);
-      });
-    } else {
-      store.cleanupSiblingsInstance(SK, BK, depth);
-    }
-  });
+  SKeys.forEach(({ BK, depth }, SK) => cleanupSiblings(store, SK, BK, depth));
 }
 
 // Needs refactoring and testing.
@@ -187,7 +198,7 @@ function DOMmutationHandler(
     }
 
     if (terminatedDOMiDs.size > 0) {
-      cleanupSiblings(store);
+      cleanupIDs(store);
       terminatedDOMiDs.clear();
     }
   } finally {
