9259b06935b52bc89a8010a15b6ec8994fe7d8f4
jalal246 <jimmy002020@gmail.com>
2023-09-05 00:39:12 +0300

    wip

--- packages/dflex-next-playground/src/pages/_document.tsx
+++ packages/dflex-next-playground/src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -9,5 +10,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
