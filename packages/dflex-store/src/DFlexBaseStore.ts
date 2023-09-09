3e3ffc65ff37892d98dc8c5d05d18f024d609ef0
jalal246 <jimmy002020@gmail.com>
2023-09-09 13:58:38 +0300

    wip

--- packages/dflex-store/src/DFlexBaseStore.ts
+++ packages/dflex-store/src/DFlexBaseStore.ts
@@ -10,13 +10,12 @@ import {
   featureFlags,
   getAnimationOptions,
   getParentElm,
-  PREFIX_TRACKER_ID,
   setFixedDimensions,
   setRelativePosition,
   TaskQueue,
-  tracker,
 } from "@dflex/utils";
 import DFlexDOMManager from "./DFlexDOMManager";
+import { getElmDOMOrThrow, assignElementID } from "./utils";
 
 type DeepRequired<T> = {
   [K in keyof T]-?: T[K] extends object
@@ -92,28 +91,6 @@ type BranchComposedCallBackFunction = (
 ) => void;
 
 type HighestContainerComposedCallBack = () => void;
-
-function getElmDOMOrThrow(id: string): HTMLElement | null {
-  let DOM = document.getElementById(id);
-
-  if (!DOM) {
-    if (__DEV__) {
-      throw new Error(
-        `Element with ID: ${id} is not found.This could be due wrong ID or missing DOM element.`,
-      );
-    }
-  }
-
-  if (!DOM || DOM.nodeType !== Node.ELEMENT_NODE) {
-    if (__DEV__) {
-      throw new Error(`Invalid HTMLElement ${DOM} is passed to registry.`);
-    }
-
-    DOM = null;
-  }
-
-  return DOM;
-}
 
 /**
  * Does the element share the same parent with the previous element in the same depth?
@@ -240,17 +217,6 @@ function submitToRegistry(
   return keys;
 }
 
-function assignElementId(DOM: HTMLElement) {
-  let { id } = DOM;
-
-  if (!id) {
-    id = tracker.newTravel(PREFIX_TRACKER_ID);
-    DOM.id = id;
-  }
-
-  return id;
-}
-
 function submitContainerChildren(
   parentDOM: HTMLElement,
   depth: number,
@@ -264,7 +230,7 @@ function submitContainerChildren(
 
   parentDOM.childNodes.forEach((DOM, i) => {
     if (DOM instanceof HTMLElement) {
-      const id = assignElementId(DOM);
+      const id = assignElementID(DOM);
 
       if (!store.registry.has(id)) {
         const elm: RegisterInputProcessed = {
@@ -386,7 +352,7 @@ class DFlexBaseStore extends DFlexDOMManager {
     }
 
     const getParentElmCallback = (parentDOM: HTMLElement) => {
-      const parentID = assignElementId(parentDOM);
+      const parentID = assignElementID(parentDOM);
 
       const isParentRegistered = this.registry.has(parentID);
 
