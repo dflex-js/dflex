324bb6dc3b00a6f9026384fc300b8bccf654a815
jalal246 <jimmy002020@gmail.com>
2023-09-16 20:32:02 +0300

    wip

--- packages/dflex-dnd-playground/tests/scroll/scroll.container.spec.ts
+++ packages/dflex-dnd-playground/tests/scroll/scroll.container.spec.ts
@@ -34,26 +34,38 @@ test.describe.parallel("Resolve scroll container correctly", async () => {
     await page2.close();
   });
 
-  test("Identify document as scroll container", async () => {
+  test("Identify document as scroll container", async ({ browserName }) => {
     const hasDataScroll = await page1.evaluate(
       () => document.body.getAttribute("data-scroll__dflex_sk_0_0") === "true",
     );
 
     expect(hasDataScroll).toBe(true);
+
+    if (browserName === "webkit") {
+      await page1.waitForTimeout(2000);
+    }
   });
 
-  test("Identify div(s) scroll container", async () => {
+  test("Identify div(s) scroll container", async ({ browserName }) => {
     const [u1, u2, u3] = (await Promise.all([
       page2.locator("#dflex_id_0"),
       page2.locator("#dflex_id_1"),
       page2.locator("#dflex_id_2"),
     ])) as Locator[];
 
+    if (browserName === "webkit") {
+      await page1.waitForTimeout(2000);
+    }
+
     const [dataScroll1, dataScroll2, dataScroll3] = await Promise.all([
       u1.getAttribute("data-scroll__dflex_sk_0_0"),
       u2.getAttribute("data-scroll__dflex_sk_0_1"),
       u3.getAttribute("data-scroll__dflex_sk_0_2"),
     ]);
+
+    if (browserName === "webkit") {
+      await page1.waitForTimeout(2000);
+    }
 
     expect(dataScroll1).toBe("true");
     expect(dataScroll2).toBe("true");
