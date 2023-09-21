54c85e1f0b13b049a0641befdbecd63273713218
jalal246 <jimmy002020@gmail.com>
2023-09-21 10:36:09 +0300

    wip

---
+++ packages/dflex-next-playground/src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as TodoContainer } from "./TodoContainer";
+export { default as TodoItem } from "./TodoItem";
--- packages/dflex-next-playground/src/pages/list/symmetric.tsx
+++ packages/dflex-next-playground/src/pages/list/symmetric.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 import React from "react";
-import TodoItem from "../../components/TodoItem";
+import { TodoItem, TodoContainer } from "../../components";
 
 const SymmetricList = () => {
   const tasks = [
@@ -14,15 +14,13 @@ const SymmetricList = () => {
   ];
 
   return (
-    <div className={`flex min-h-screen flex-col items-center p-6`}>
-      <ul className="bg-green-50 border-2 border-blue-50 rounded p-4">
-        {tasks.map(({ task, id }) => (
-          <TodoItem id={id} key={id} opts={{ scroll: { enable: false } }}>
-            {task}
-          </TodoItem>
-        ))}
-      </ul>
-    </div>
+    <TodoContainer>
+      {tasks.map(({ task, id }) => (
+        <TodoItem id={id} key={id} opts={{ scroll: { enable: false } }}>
+          {task}
+        </TodoItem>
+      ))}
+    </TodoContainer>
   );
 };
 
