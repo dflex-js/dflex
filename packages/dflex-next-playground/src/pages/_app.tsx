0f293e6fde0134ac7b6fe119773df12214e66c26
Jalal <jimmy002020@gmail.com>
2023-09-29 13:11:41 +0300

    Implement clean-up of store instances and events when there are no registered elements (#720)

--- packages/dflex-next-playground/src/pages/_app.tsx
+++ packages/dflex-next-playground/src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { store } from "@dflex/dnd";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Not required but for testing.
+  React.useEffect(() => {
+    return () => {
+      store.destroy();
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
