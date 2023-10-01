ad3fc9009f538c942aeecea22a19d770bc3bc9e3
jalal246 <jimmy002020@gmail.com>
2023-10-01 22:05:30 +0300

    wip

--- packages/dflex-vue-playground/src/router/index.ts
+++ packages/dflex-vue-playground/src/router/index.ts
@@ -8,14 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
