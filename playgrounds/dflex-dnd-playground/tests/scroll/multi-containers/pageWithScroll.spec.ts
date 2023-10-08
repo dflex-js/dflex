b178877e057214954e75223c79c2d7317fc1867e
jalal246 <jimmy002020@gmail.com>
2023-10-08 17:36:24 +0300

    wip

--- playgrounds/dflex-dnd-playground/tests/scroll/multi-containers/pageWithScroll.spec.ts
+++ playgrounds/dflex-dnd-playground/tests/scroll/multi-containers/pageWithScroll.spec.ts
@@ -15,6 +15,7 @@ import {
   moveDragged,
   TransformTimeout,
   assertChildrenOrderIDs,
+  invokeKeyboardAndAssertEmittedMsg,
 } from "dflex-e2e-utils";
 
 test.describe
@@ -24,10 +25,10 @@ test.describe
   let activeBrowser: Browser;
 
   let elementIDsA: string[];
-  //   let elementIDsB: string[];
-
   let elementsA: Locator[];
-  //   let elementsB: Locator[];
+
+  let mergedIDs: string[];
+  let mergedElements: Locator[];
 
   let parentA: Locator;
   let parentB: Locator;
@@ -160,11 +161,63 @@ test.describe
       (_, index) => `list-b-${startIdxB + index}`,
     );
 
-    const mergedIDs = [...movedIDsElmA, ...iDsElmB];
+    mergedIDs = [...movedIDsElmA, ...iDsElmB];
 
     await Promise.all([
       assertChildrenOrderIDs(parentB, mergedIDs),
       assertDefaultChildrenIndex(parentB),
+    ]);
+  });
+
+  test("Move elm-a-4 down to the container-b bottom", async () => {
+    await getDraggedRect(elementsA[3]);
+    await moveDragged(-1, 600);
+    await page.waitForTimeout(2000);
+    await moveDragged(-1, 420);
+    await page.waitForTimeout(2000);
+    await page.dispatchEvent(elementIDsA[0], "mouseup", {
+      button: 0,
+      force: true,
+    });
+  });
+
+  test("elements in container-b have the correct position", async () => {
+    mergedElements = mergedIDs
+      .filter((id) => id !== "list-a-4")
+      .map((id) => page.locator(`[id='${id}']`));
+
+    await Promise.all(
+      mergedElements.map(async (element) => {
+        await expect(element).toHaveCSS(
+          "transform",
+          "matrix(1, 0, 0, 1, 0, -59.1875)",
+          TransformTimeout,
+        );
+      }),
+    );
+  });
+
+  test("Trigger key `c` to commit the transformed elements", async () => {
+    await invokeKeyboardAndAssertEmittedMsg([
+      "list-a-3",
+      "list-a-2",
+      "list-a-1",
+      "list-b-1",
+      "list-b-2",
+      "list-b-3",
+      "list-b-4",
+      "list-b-5",
+      "list-b-6",
+      "list-b-7",
+      "list-b-8",
+      "list-b-9",
+      "list-b-10",
+      "list-b-11",
+      "list-b-12",
+      "list-b-13",
+      "list-b-14",
+      "list-b-15",
+      "list-a-4",
     ]);
   });
 });
