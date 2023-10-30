9eae767db31ea118192e1cb8545e9f81494439d2
jalal246 <jimmy002020@gmail.com>
2023-10-30 12:21:15 +0200

    wip

--- playgrounds/dflex-dnd-playground/tests/features/serialization.spec.ts
+++ playgrounds/dflex-dnd-playground/tests/features/serialization.spec.ts
@@ -29,58 +29,7 @@ test.describe("Visible elements have transformation after loading in the the mid
   let visibleDFlexElements: DFlexSerializedElements;
   let invisibleDFlexElements: DFlexSerializedElements;
 
-  test.beforeAll(async ({ browser, browserName }) => {
-    activeBrowser = browser;
-
-    context = await activeBrowser.newContext();
-    page = await context.newPage();
-    initialize(page, browserName, 50);
-    await page.goto("/extended");
-
-    const elementSelectors = Array.from(
-      { length: 100 },
-      (_, index) => `[id='${index + 1}-extended']`,
-    );
-
-    elements = await Promise.all(
-      elementSelectors.map((selector) => page.locator(selector)),
-    );
-  });
-
-  test.afterAll(async () => {
-    await page.close();
-    await context.close();
-    // await activeBrowser.close();
-  });
-
-  test.describe("in the middle", () => {
-    test.beforeAll(async ({ browserName }) => {
-      await page.evaluate(() => {
-        window.scrollTo(0, document.body.scrollHeight / 2);
-      });
-
-      // Reloading in another browsers will move scroll to the start.
-      if (browserName === "chromium") {
-        await page.reload();
-      }
-
-      await page.waitForTimeout(1000);
-
-      dflexElements = await getSerializedElementsAfterKeyPress();
-      await page.waitForTimeout(1000);
-
-      // Slices elements from index 50 to 60 (inclusive)
-      visibleDFlexElements = dflexElements.slice(50, 61);
-      visibleElements = elements.slice(50, 61);
-
-      // Generates elements from index 0 to 44 and from 66 to the end but with 5
-      // elements margin to take into consideration the threshold.
-      invisibleDFlexElements = [
-        ...dflexElements.slice(0, 45),
-        ...dflexElements.slice(66),
-      ];
-    });
-
+  async function launchTestProcess() {
     test("DFlex Rect matches elements live Rect", async () => {
       const boundingClientRectPromise = visibleElements.map((l) =>
         l.boundingBox(),
@@ -147,5 +96,116 @@ test.describe("Visible elements have transformation after loading in the the mid
         });
       });
     });
+  }
+
+  test.beforeAll(async ({ browser, browserName }) => {
+    activeBrowser = browser;
+
+    context = await activeBrowser.newContext();
+    page = await context.newPage();
+    initialize(page, browserName, 50);
+    await page.goto("/extended");
+
+    const elementSelectors = Array.from(
+      { length: 100 },
+      (_, index) => `[id='${index + 1}-extended']`,
+    );
+
+    elements = await Promise.all(
+      elementSelectors.map((selector) => page.locator(selector)),
+    );
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+    await context.close();
+    // await activeBrowser.close();
+  });
+
+  test.describe("in the top", async () => {
+    test.beforeAll(async ({ browserName }) => {
+      await page.evaluate(() => {
+        window.scrollTo(0, 0);
+      });
+
+      // Reloading in another browsers will move scroll to the start.
+      if (browserName === "chromium") {
+        await page.reload();
+      }
+
+      await page.waitForTimeout(1000);
+
+      dflexElements = await getSerializedElementsAfterKeyPress();
+      await page.waitForTimeout(1000);
+
+      // Slices elements from index 0 to 10 (inclusive)
+      visibleDFlexElements = dflexElements.slice(0, 11);
+      visibleElements = elements.slice(0, 11);
+
+      // Generates elements from index 11 to the end but with 5
+      // elements margin to take into consideration the threshold.
+      invisibleDFlexElements = dflexElements.slice(16);
+    });
+
+    await launchTestProcess();
+  });
+
+  test.describe("in the middle", async () => {
+    test.beforeAll(async ({ browserName }) => {
+      await page.evaluate(() => {
+        window.scrollTo(0, document.body.scrollHeight / 2);
+      });
+
+      // Reloading in another browsers will move scroll to the start.
+      if (browserName === "chromium") {
+        await page.reload();
+      }
+
+      await page.waitForTimeout(1000);
+
+      dflexElements = await getSerializedElementsAfterKeyPress();
+      await page.waitForTimeout(1000);
+
+      // Slices elements from index 50 to 60 (inclusive)
+      visibleDFlexElements = dflexElements.slice(50, 61);
+      visibleElements = elements.slice(50, 61);
+
+      // Generates elements from index 0 to 44 and from 66 to the end but with 5
+      // elements margin to take into consideration the threshold.
+      invisibleDFlexElements = [
+        ...dflexElements.slice(0, 45),
+        ...dflexElements.slice(66),
+      ];
+    });
+
+    await launchTestProcess();
+  });
+
+  test.describe("in the end", async () => {
+    test.beforeAll(async ({ browserName }) => {
+      await page.evaluate(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+
+      // Reloading in another browsers will move scroll to the start.
+      if (browserName === "chromium") {
+        await page.reload();
+      }
+
+      await page.waitForTimeout(1000);
+
+      dflexElements = await getSerializedElementsAfterKeyPress();
+      await page.waitForTimeout(1000);
+
+      // Slices elements from index 90 to 100 (inclusive)
+      visibleDFlexElements = dflexElements.slice(90, dflexElements.length);
+      visibleElements = elements.slice(90, elements.length);
+
+      // Generates elements from index 0 to 85 with 5 elements margin to take
+      // into consideration the threshold.
+      invisibleDFlexElements = dflexElements.slice(0, 85);
+    });
+
+    await launchTestProcess();
   });
 });
